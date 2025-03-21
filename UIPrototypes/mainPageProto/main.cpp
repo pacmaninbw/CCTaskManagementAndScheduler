05f08aaa0ccfa39e92d46af3e663ef78beba369c
pacmaninbw <paul.chernick@chernicksw.com>
2025-03-21 07:07:04 -0700

    Removed template files

--- UIPrototypes/mainPageProto/main.cpp
+++ UIPrototypes/mainPageProto/main.cpp
@@ -1,7 +1,6 @@
 #include "taskTodoListUI.h"
 #include "scheduleUI.h"
 #include "notesUI.h"
-#include "pacswBaseDialog.h"
 
 #include <QApplication>
 
@@ -10,6 +9,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     taskTodoListDialog t;
     t.show();
-    PACSWBaseDialog p;
     return a.exec();
 }
