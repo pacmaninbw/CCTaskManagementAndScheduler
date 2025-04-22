005c8cd992e12a29384436e365425bbb0c83a262
pacmaninbw <paul.chernick@chernicksw.com>
2025-04-22 07:55:18 -0700

    Merged all SQL script files into one master SQL script file.

--- Development/Prototypes/database/PlannerTaskScheduleDB.sql
+++ Development/Prototypes/database/PlannerTaskScheduleDB.sql
@@ -307,4 +307,598 @@ CREATE TABLE IF NOT EXISTS `PlannerTaskScheduleDB`.`Notes` (
       ON UPDATE RESTRICT
 );
     
+-- -----------------------------------------------------
+-- Stored Functions
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- function findTaskStatusValueByLabel
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findTaskStatusValueByLabel`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findTaskStatusValueByLabel`(
+    StatusStr VARCHAR(20)
+) RETURNS INT
+DETERMINISTIC
+BEGIN
+    
+    SET @TaskStatusKey = 0;
+    
+    SELECT TaskStatus.idTaskStatus INTO @TaskStatusKey
+        FROM TaskStatus
+        WHERE TaskStatus.StatusStr = StatusStr;
+    IF @TaskStatusKey IS NULL THEN
+        SET @TaskStatusKey = 0;
+    END IF;
+
+    RETURN @TaskStatusKey;
+    
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- function findTaskStatusLabelByValue
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findTaskStatusLabelByValue`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findTaskStatusLabelByValue`(
+    StatusKey INT
+) RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    
+    SET @TaskStatusLabel = ``;
+    
+    SELECT TaskStatus.StatusStr INTO @TaskStatusLabel
+        FROM TaskStatus
+        WHERE TaskStatus.idTaskStatus = StatusStr;
+    IF @TasTaskStatusLabelkStatusKey IS NULL THEN
+        SET @TaskStatusLabel = 'Not Started';
+    END IF;
+
+    RETURN @TaskStatusLabel;
+    
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- function findScheduleItemTypeLabelByValue
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findScheduleItemTypeLabelByValue`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findScheduleItemTypeLabelByValue`(
+    StatusKey INT
+) RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    
+    SET @ScheduleItemTypeLabel = ``;
+    
+    SELECT ScheduleItemType.StatusStr INTO @ScheduleItemTypeLabel
+        FROM ScheduleItemType
+        WHERE ScheduleItemType.idScheduleItemType = StatusStr;
+    IF @TasScheduleItemTypeLabelkStatusKey IS NULL THEN
+        SET @ScheduleItemTypeLabel = 'Not Started';
+    END IF;
+
+    RETURN @ScheduleItemTypeLabel;
+    
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- function findScheduleItemTypeValueByLabel
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findScheduleItemTypeValueByLabel`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findScheduleItemTypeValueByLabel`(
+    StatusStr VARCHAR(20)
+) RETURNS INT
+DETERMINISTIC
+BEGIN
+    
+    SET @ScheduleItemTypeKey = 0;
+    
+    SELECT ScheduleItemType.idScheduleItemType INTO @ScheduleItemTypeKey
+        FROM ScheduleItemType
+        WHERE ScheduleItemType.StatusStr = StatusStr;
+    IF @ScheduleItemTypeKey IS NULL THEN
+        SET @ScheduleItemTypeKey = 0;
+    END IF;
+
+    RETURN @ScheduleItemTypeKey;
+    
+END$$
+
+DELIMITER ;
+
+
+-- -----------------------------------------------------
+-- function findUserIDKeyByLoginName
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findUserIDKeyByLoginName`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findUserIDKeyByLoginName`(
+    LoginName VARCHAR(45)
+) RETURNS INT
+DETERMINISTIC
+BEGIN
+
+    SET @UserIDKey = 0;
+
+    SELECT LoginAndPassword.UserID INTO @UserIDKey
+        FROM LoginAndPassword
+        WHERE LoginAndPassword.LoginName = LoginName;
+    IF @UserIDKey IS NULL THEN
+        SET @UserIDKey = 0;
+    END IF;
+
+    RETURN @UserIDKey;
+    
+END$$
+
+DELIMITER;
+
+-- -----------------------------------------------------
+-- function findUserIDKeyByFullName
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `PlannerTaskScheduleDB`.`findUserIDKeyByFullName`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `findUserIDKeyByFullName`(
+    LastName VARCHAR(45),
+    FirstName TINYTEXT,
+    MiddleInitial TINYTEXT
+) RETURNS INT
+DETERMINISTIC
+BEGIN
+
+    SET @UserIDKey = 0;
+
+    SELECT UserProfile.idUserProfile INTO @UserIDKey
+        FROM UserProfile
+        WHERE UserProfile.LastName = LastName AND
+            UserProfile.FirstName = FirstName AND
+            UserProfile.MiddleInitial = MiddleInitial;
+    IF @UserIDKey IS NULL THEN
+        SET @UserIDKey = 0;
+    END IF;
+
+    RETURN @UserIDKey;
+    
+END$$
+
+DELIMITER;
+
+-- -----------------------------------------------------
+-- function isValidLoginAndPassword
+-- -----------------------------------------------------
+
+USE `PlannerTaskScheduleDB`;
+DROP function IF EXISTS `isValidLoginAndPassword`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE FUNCTION `isValidLoginAndPassword`
+(
+    LoginName VARCHAR(45),
+    HashedPassWord TINYTEXT
+)
+RETURNS TINYINT
+DETERMINISTIC
+BEGIN
+
+    SET @UserIDKey = 0;
+    SET @isValid = 1;
+
+    SELECT LoginAndPassword.UserID INTO @UserIDKey
+        FROM LoginAndPassword
+        WHERE LoginAndPassword.LoginName = LoginName AND
+            LoginAndPassword.HashedPassWord = HashedPassWord;
+
+    IF @UserIDKey IS NULL THEN
+        SET @isValid = 0;
+    END IF;
+
+    RETURN @isValid;
+
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- Stored Procedures
+-- -----------------------------------------------------
+
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `addNewUserPreferences`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `addNewUserPreferences`
+(
+	IN UserID INT UNSIGNED
+)
+BEGIN
+
+	INSERT INTO PlatformIndepenentPreferences (
+		PlatformIndepenentPreferences.UserID,
+        PlatformIndepenentPreferences.ScheduleDayStart,
+        PlatformIndepenentPreferences.ScheduleDayEnd
+    )
+    
+    VALUES (UserID, '8:30:00', '17:00:00');
+    
+END$$
+
+DELIMITER ;
+
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `addNewUserLoginAndPassword`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `addNewUserLoginAndPassword`
+(
+	IN UserID INT UNSIGNED,
+    IN LoginName VARCHAR(45),
+    IN HashedPassWord TINYTEXT
+)
+BEGIN
+
+	INSERT INTO LoginAndPassword
+		(
+			LoginAndPassword.UserID,
+            LoginAndPassword.LoginName,
+            LoginAndPassword.HashedPassWord
+		)
+		VALUES (UserID, LoginName, HashedPassWord);
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `addNewUser`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `addNewUser`
+	(
+        IN LastName VARCHAR(45),
+		IN FirstName TINYTEXT,
+		IN MiddleInitial TINYTEXT,
+	    IN LoginName VARCHAR(45),
+		IN HashedPassWord TINYTEXT
+    )
+BEGIN
+
+	INSERT INTO UserProfile
+		(
+			UserProfile.LastName,
+            UserProfile.FirstName,
+            UserProfile.MiddleInitial
+        )
+		VALUES (LastName, FirstName, MiddleInitial);
+        
+        SET @NewUserID := LAST_INSERT_ID();
+        
+        CALL addNewUserLoginAndPassword(@NewUserID, LoginName, HashedPassWord);
+        
+        CALL addNewUserPreferences(@NewUserID);
+
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `createTaskDates`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `createTaskDates`
+(
+	TaskID INT UNSIGNED,
+    RequiredDelivery DATE,
+    ScheduledStart DATE
+)
+BEGIN
+	SET @CreationDate := curdate();
+    
+    INSERT INTO Dates
+		(
+			Dates.TaskID,
+            Dates.CreatedOn,
+            Dates.RequiredDelivery,
+            Dates.ScheduledStart
+		)
+		VALUES (
+			TaskID,
+            @CreationDate,
+            RequiredDelivery,
+            ScheduledStart
+		);
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `createTaskEffort`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `createTaskEffort`
+(
+	IN TaskID INT UNSIGNED,
+    IN EstimatedEffortHours INT UNSIGNED
+)
+BEGIN
+
+	INSERT INTO Effort
+		(
+			Effort.TaskID,
+            Effort.EstimatedEffortHours,
+            Effort.ActualEffortHours
+		)
+        VALUES
+        (
+			TaskID,
+            EstimatedEffortHours,
+            0.0
+        );
+    
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `createTask`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `createTask`
+(
+	IN CreatedBy INT UNSIGNED,
+    IN Description TINYTEXT,
+    IN ParentTask INT UNSIGNED,
+    IN EstimatedEffortHours INT UNSIGNED,
+    IN PriorityInAllTasks INT UNSIGNED,
+    IN RequiredDelivery DATE,
+    IN ScheduledStart DATE
+)
+BEGIN
+
+	SET @TaskStatusValue = findTaskStatusValueByLabel('Not Started');
+    
+	INSERT INTO Tasks
+		(
+			Tasks.CreatedBy,
+            Tasks.AsignedTo,
+            Tasks.Description,
+            Tasks.PriorityInAllTasks,
+            Tasks.Status
+        )
+		VALUES
+		(
+			CreatedBy,
+            CreatedBy,
+            Description,
+            PriorityInAllTasks,
+            @TaskStatusValue
+		);
+        
+		SET @NewTaskID := LAST_INSERT_ID();
+        
+        CALL createTaskDates(
+			@NewTaskID,
+            RequiredDelivery,
+            ScheduledStart
+		);
+        
+        CALL createTaskEffort
+        (
+			@NewTaskID,
+            EstimatedEffortHours
+        );
+
+    IF ParentTask IS NOT NULL AND ParentTask > 0 THEN
+        UPDATE Tasks 
+            SET Tasks.ParentTask = ParentTask
+            WHERE Tasks.idTasks = @NewTaskID;
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `createUserTaskPriority`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `createUserTaskPriority`
+(
+	IN UserID INT UNSIGNED,
+    IN TaskID INT UNSIGNED,
+    IN SchedulePriorityGroup INT UNSIGNED,
+    IN PriorityInGroup INT UNSIGNED
+)
+BEGIN
+
+	INSERT INTO UserPriority
+		(
+			UserPriority.TaskID,
+            UserPriority.UserID,
+            UserPriority.Level,
+            UserPriority.PriorityInLevel
+		)
+        VALUES
+        (
+			TaskID,
+            UserID,
+            SchedulePriorityGroup,
+            PriorityInGroup
+        );
+END$$
+
+DELIMITER ;
+
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `creatNote`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `creatNote`
+(
+	IN UserID INT UNSIGNED,
+    IN Content VARCHAR(1024)
+)
+BEGIN
+
+	SET @NotationDateTime := now();
+    
+    INSERT INTO Notes
+		(
+			Notes.UserID,
+            Notes.NotationDateTime,
+            Notes.Content
+		)
+		VALUES
+		(
+			UserID,
+            @NotationDateTime,
+            Content
+        );
+
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `createGoal`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `createGoal`
+(
+	IN UserID INT UNSIGNED,
+    IN Description TINYTEXT,
+    IN Priority INT,
+    IN ParentGoal INT UNSIGNED
+)
+BEGIN
+
+	INSERT INTO Goals
+		(
+			Goals.UserID,
+            Goals.Description
+        )
+        VALUES
+        (
+			UserID,
+            Description
+        );
+        
+	SET @idGoals  := LAST_INSERT_ID();
+    
+	IF Priority IS NOT NULL AND Priority > 0 THEN
+        UPDATE Goals 
+            SET Goals.Priority = Priority
+            WHERE Goals.idTasks = @idGoals;
+    END IF;
+
+	IF ParentGoal IS NOT NULL AND ParentGoal > 0 THEN
+        UPDATE Goals 
+            SET Goals.ParentGoal = ParentGoal
+            WHERE Goals.idTasks = @idGoals;
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `accumulateTaskEffort`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `accumulateTaskEffort`
+(
+	IN TaskID INT UNSIGNED,
+    IN ActualEffortHours FLOAT
+)
+BEGIN
+
+	SELECT ActualEffortHours
+		INTO @AccumlatedEffort
+        FROM Effort
+        WHERE Effort.TaskID = TaskID;
+        
+	SET @AccumlatedEffort = @AccumlatedEffort + ActualEffortHours;
+    
+    UPDATE Effort 
+		SET Effort.ActualEffortHours = @AccumlatedEffort
+        WHERE Effort.TaskID = TaskID;
+        
+END$$
+
+DELIMITER ;
+
+USE `PlannerTaskScheduleDB`;
+DROP procedure IF EXISTS `addTaskDependency`;
+
+DELIMITER $$
+USE `PlannerTaskScheduleDB`$$
+CREATE PROCEDURE `addTaskDependency`
+(
+	IN TaskID INT UNSIGNED,
+    IN Dependency INT UNSIGNED
+)
+BEGIN
+
+	INSERT INTO Dependencies
+		(
+			Dependencies.TaskID,
+            Dependencies.Dependency
+        )
+        VALUES
+        (
+			TaskID,
+            Dependency
+        );
+END$$
+
+DELIMITER ;
+
+
+
 COMMIT;
