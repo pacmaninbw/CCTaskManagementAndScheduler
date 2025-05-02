f2b208b97adcdac2f24331d5fd51e090f008369c
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-02 06:08:57 -0700

    Added dbConnectionParameters.h but redacted the username and password.

---
+++ Development/Prototypes/CPPDataBase/dbConnectionParameters.h
@@ -0,0 +1,10 @@
+
+#ifndef DBCONNECTIONPARAMETERS_H_
+#define DBCONNECTIONPARAMETERS_H_
+
+static const char* MySQLRootUser = "REDACTED";
+static const char* MySQLRootPassword = "REDACTED";
+static const char* PlannerDB = "PlannerTaskScheduleDB";
+
+#endif // DBCONNECTIONPARAMETERS_H_
+
