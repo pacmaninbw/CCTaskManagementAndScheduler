d9c02d64655c0833130f154cdc09889d5bc8bb22
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-03 08:26:39 -0700

    Implemented the interface to the database for tasks, but there are runtime errors.

--- Development/Prototypes/CPPDataBase/DBInterface.cpp
+++ Development/Prototypes/CPPDataBase/DBInterface.cpp
@@ -10,13 +10,17 @@
 #include <boost/mysql/results.hpp>
 #include <boost/mysql/row_view.hpp>
 #include <boost/mysql/with_params.hpp>
-
+#include <cstdint>
 #include "dbConnectionParameters.h"
 #include "DBInterface.h"
 #include <exception>
 #include <string>
 #include <string_view>
 
+/*
+ * The database connection information will not change during the entire
+ * execution of the program.
+ */
 static boost::mysql::connect_params dbConnectionParameters;
 
 DBInterface::DBInterface()
@@ -27,6 +31,11 @@ DBInterface::DBInterface()
     dbConnectionParameters.database = PlannerDB;
 }
 
+/*
+ * To limit the includes for Boost/mysql.hpp and Boost/asio.hpp to this file
+ * all co-routines are static functions. The boost mysql and asio header files
+ * are very large and incur major build times.
+ */
 static boost::asio::awaitable<void> coro_addUser(
     std::string_view lastName,
     std::string_view firstName,
@@ -50,11 +59,112 @@ static boost::asio::awaitable<void> coro_addUser(
 }
 
 void DBInterface::addUser(
-    std::string_view lastName,
-    std::string_view firstName,
-    std::string_view middleInitial,
-    std::string_view loginName,
-    std::string_view hashedPassword
+    std::string_view& lastName,
+    std::string_view& firstName,
+    std::string_view& middleInitial,
+    std::string_view& loginName,
+    std::string_view& hashedPassword
+)
+{
+    throwExceptionIfNotValidAddUser(lastName, firstName, loginName, hashedPassword);
+
+    boost::asio::io_context ctx;
+
+    boost::asio::co_spawn(ctx,
+        [lastName, firstName, middleInitial, loginName, hashedPassword] {
+            return coro_addUser(lastName, firstName, middleInitial, loginName, hashedPassword); },
+        [](std::exception_ptr ptr) {
+            if (ptr)
+            {
+                std::rethrow_exception(ptr);
+            }
+        }
+    );
+
+    ctx.run();
+}
+
+
+static boost::asio::awaitable<void> coro_addUserTask(
+    KeyType userID,
+    std::string_view description,
+    KeyType parentTask,
+    unsigned int estimatedEffortHours,
+    unsigned int priorityInAllTasks,
+    std::string_view requiredDelivery,
+    std::string_view scheduledStart
+)
+{
+
+    boost::mysql::any_connection conn(co_await boost::asio::this_coro::executor);
+
+    co_await conn.async_connect(dbConnectionParameters);
+
+    boost::mysql::results result;
+    co_await conn.async_execute(
+        boost::mysql::with_params("CALL createTask({}, {}, {}, {}, {}, {}. {})", 
+            userID, description, parentTask, estimatedEffortHours, priorityInAllTasks,
+            requiredDelivery, scheduledStart),
+        result
+    );
+
+    co_await conn.async_close();
+}
+
+void DBInterface::addUserTaskWithUserID(
+    KeyType userID,
+    std::string_view description,
+    KeyType parentTask,
+    unsigned int estimatedEffortHours,
+    unsigned int priorityInAllTasks,
+    std::string_view requiredDelivery,
+    std::string_view scheduledStart
+)
+{
+    boost::asio::io_context ctx;
+
+    boost::asio::co_spawn(ctx,
+        [userID, description, parentTask, estimatedEffortHours, priorityInAllTasks, requiredDelivery, scheduledStart] {
+            return coro_addUserTask(userID, description, parentTask, estimatedEffortHours, priorityInAllTasks, requiredDelivery, scheduledStart); },
+        [](std::exception_ptr ptr) {
+            if (ptr)
+            {
+                std::rethrow_exception(ptr);
+            }
+        }
+    );
+
+    ctx.run();
+}
+
+void DBInterface::addUserTaskWithLoginName()
+{
+}
+
+void DBInterface::stringViewValidationOrErrorMsg(
+    std::string_view& contents,
+    std::string_view& strName,
+    std::string& eMsg,
+    unsigned int& errorCount
+) const
+{
+    if (contents.empty() || contents.size() == 0)
+    {
+        if (errorCount)
+        {
+            eMsg += ", ";;
+        }
+
+        eMsg += strName;
+        errorCount++;
+    }
+}
+
+void DBInterface::throwExceptionIfNotValidAddUser(
+    std::string_view &lastName,
+    std::string_view &firstName,
+    std::string_view &loginName,
+    std::string_view &hashedPassword
 )
 {
     unsigned int errorCount = 0;
@@ -85,47 +195,5 @@ void DBInterface::addUser(
         }
         std::runtime_error addUserException(addUserErrorMessage);
         throw addUserException;
-    }
-
-    boost::asio::io_context ctx;
-
-    boost::asio::co_spawn(ctx,
-        [lastName, firstName, middleInitial, loginName, hashedPassword] {
-            return coro_addUser(lastName, firstName, middleInitial, loginName, hashedPassword); },
-        [](std::exception_ptr ptr) {
-            if (ptr)
-            {
-                std::rethrow_exception(ptr);
-            }
-        }
-    );
-
-    ctx.run();
-}
-
-void DBInterface::addUserTaskWithUserID()
-{
-}
-
-void DBInterface::addUserTaskWithLoginName()
-{
-}
-
-void DBInterface::stringViewValidationOrErrorMsg(
-    std::string_view& contents,
-    std::string_view& strName,
-    std::string& eMsg,
-    unsigned int& errorCount
-) const
-{
-    if (contents.empty() || contents.size() == 0)
-    {
-        if (errorCount)
-        {
-            eMsg += ", ";;
-        }
-
-        eMsg += strName;
-        errorCount++;
     }
 }
--- Development/Prototypes/CPPDataBase/DBInterface.h
+++ Development/Prototypes/CPPDataBase/DBInterface.h
@@ -1,21 +1,28 @@
 #ifndef DBINTERFACE_H_
 #define DBINTERFACE_H_
 
+#include <cstdint>
 #include <string_view>
+
+using KeyType = uint64_t;
 
 class DBInterface
 {
 public:
     DBInterface();
     ~DBInterface() = default;
-    void addUser(std::string_view lastName, std::string_view firstName,
-        std::string_view middleInitial, std::string_view loginName, std::string_view hashedPassword);
-    void addUserTaskWithUserID();
+    void addUser(std::string_view& lastName, std::string_view& firstName,
+        std::string_view& middleInitial, std::string_view& loginName, std::string_view& hashedPassword);
+    void addUserTaskWithUserID(KeyType userID, std::string_view description, KeyType parentTask,
+        unsigned int estimatedEffortHours, unsigned int priorityInAllTasks, std::string_view requiredDelivery,
+        std::string_view scheduledStart);
     void addUserTaskWithLoginName();
 
 private:
     void stringViewValidationOrErrorMsg(std::string_view& contents,
         std::string_view& strName, std::string& eMsg, unsigned int& errorCount) const;
+    void throwExceptionIfNotValidAddUser(std::string_view& lastName, std::string_view& firstName,
+        std::string_view& loginName, std::string_view& hashedPassword);
 };
 
 #endif // DBINTERFACE_H_
