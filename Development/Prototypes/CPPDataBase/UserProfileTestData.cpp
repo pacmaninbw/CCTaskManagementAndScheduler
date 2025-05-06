1c1add0afacbced2ab59069dc671eab82e3aa53c
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-05 17:28:48 -0700

    The unit testing of adding users to the database now uses the user class instead of a temporary data structure.

--- Development/Prototypes/CPPDataBase/UserProfileTestData.cpp
+++ Development/Prototypes/CPPDataBase/UserProfileTestData.cpp
@@ -1,26 +1,18 @@
 #include "DBInterface.h"
+#include "InMemUser.h"
 #include <iostream>
 #include <string_view>
 #include <vector>
 #include "UserProfileTestData.h"
 
-struct UserProfileTestData
+static std::vector<InMemUser> userProfileTestData = 
 {
-    std::string_view lastName;
-    std::string_view firstName;
-    std::string_view middleInitial;
-    std::string_view loginName;
-    std::string_view hashedPassword;
-};
-
-static std::vector<struct UserProfileTestData> userProfileTestData = 
-{
-    {"Chernick", "Paul", "A", "ChernickPaulA", "WhereforeWhyFore1234"},
-    {"Chernick", "Nina", "L", "ChernickNinaL", "PrimaLinguaLTD"},
-    {"Chernick", "Dina", "B", "ChernickDinaB", "At@LawLegalEagle"},
-    {"Shiminovics", "Eitan", "I", "ShimonvicsEitanI", "justAKid!!"},
-    {"PacMan", "IN", "BW", "pacmaninbw", "m0rNingGlory"},
-    {"Black", "Patrick", "A", "BlackPatrickA", "#n0oG13$TsS"}
+    {"Chernick", "Paul", "A", "paul.chernick@chernicksw.com"},
+    {"Chernick", "Nina", "L", "ChernickNinaL@gmail.com"},
+    {"Chernick", "Dina", "B", "ChernickDinaB@gmail.com"},
+    {"Shiminovics", "Eitan", "I", "ShimonvicsEitanI@gmail.com"},
+    {"PacMan", "IN", "BW", "pacmaninbw@gmail.com"},
+    {"Black", "Patrick", "A", "BlackPatrickA@gmail.com"}
 };
 
 
@@ -28,6 +20,7 @@ void loadUserProfileTestDataIntoDatabase(DBInterface& dbInterface)
 {
     for (auto user: userProfileTestData)
     {
-        dbInterface.addUser(user.lastName, user.firstName, user.middleInitial, user.loginName, user.hashedPassword);
+        std::cout << user << "\n\n";
+        dbInterface.addUser(user);
     }
 }
