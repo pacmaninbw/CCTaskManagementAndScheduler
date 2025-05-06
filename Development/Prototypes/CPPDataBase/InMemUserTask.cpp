d278b0ce1bd26249ddb7944f4a1fd65639a0d17d
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-06 09:06:54 -0700

    Necessary task constructors and some implementation for task added.

--- Development/Prototypes/CPPDataBase/InMemUserTask.cpp
+++ Development/Prototypes/CPPDataBase/InMemUserTask.cpp
@@ -1,8 +1,84 @@
+#include <ctime>
+#include "InMemUser.h"
 #include "InMemUserTask.h"
+#include <iostream>
+#include <string>
 
 InMemUserTask::InMemUserTask()
+:   taskID{0},
+    createdByUid{0},
+    assignedToUid{0},
+    description{"descriptionIn"},
+    parentTaskID{0},
+    status{TaskStatus::Not_Started},
+    percentageComplete{0.0},
+    createdOnDate{""},
+    requiredDeliveryDate{""},
+    scheduledStartDate{""},
+    actualStartDate{""},
+    estimatedCompletionDate{""},
+    completedDate{""},
+    estimatedEffortHours{0},
+    actualEffortHours{0.0},
+    priorityGroup{1},
+    priority{1}
 {
 
+}
+
+InMemUserTask::InMemUserTask(
+    InMemUser &creator, std::string &descriptionIn, unsigned int estimatedHoursEffort, std::string dueDate, std::string startDate,
+    InMemUserTask *parentTaskp, TaskStatus statusIn, unsigned int majorPriority, unsigned int minorPriority)
+    : taskID{0},
+    createdByUid{creator.getUserID()},
+    assignedToUid{creator.getUserID()},
+    description{descriptionIn},
+    parentTaskID{0},
+    status{statusIn},
+    percentageComplete{0.0},
+    createdOnDate{""},
+    requiredDeliveryDate{dueDate},
+    scheduledStartDate{startDate},
+    actualStartDate{""},
+    estimatedCompletionDate{""},
+    completedDate{""},
+    estimatedEffortHours{estimatedHoursEffort},
+    actualEffortHours{0.0},
+    priorityGroup{majorPriority},
+    priority{minorPriority}
+{
+    setCreationDate();
+
+    if (parentTaskp)
+    {
+        parentTaskID = parentTaskp->parentTaskID;
+    }
+}
+
+InMemUserTask::InMemUserTask(
+    std::size_t taskIDfromDB, std::size_t createdByUidfromDb, std::size_t assignedToUidfromDb, std::string descriptionfromDb,
+    std::size_t parentTaskIDfromDb, unsigned int statusfromDb, double percentageCompletefromDb, std::string createdOnDatefromDb,
+    std::string requiredDeliveryDatefromDb, std::string scheduledStartDatefromDb, std::string actualStartDatefromDb,
+    std::string estimatedCompletionDatefromDb, std::string completedDatefromDb, unsigned int estimatedEffortHoursfromDb,
+    double actualEffortHoursfromDb, unsigned int priorityGroupfromDb, unsigned int priorityfromDb)
+    : taskID{taskIDfromDB},
+    createdByUid{createdByUidfromDb},
+    assignedToUid{assignedToUidfromDb},
+    description{descriptionfromDb},
+    parentTaskID{parentTaskIDfromDb},
+    status{statusFromInt(statusfromDb)},
+    percentageComplete{percentageCompletefromDb},
+    createdOnDate{createdOnDatefromDb},
+    requiredDeliveryDate{requiredDeliveryDatefromDb},
+    scheduledStartDate{scheduledStartDatefromDb},
+    actualStartDate{actualStartDatefromDb},
+    estimatedCompletionDate{estimatedCompletionDatefromDb},
+    completedDate{completedDatefromDb},
+    estimatedEffortHours{estimatedEffortHoursfromDb},
+    actualEffortHours{actualEffortHoursfromDb},
+    priorityGroup{priorityGroupfromDb},
+    priority{priorityfromDb}
+{
 }
 
 InMemUserTask::~InMemUserTask()
@@ -28,3 +104,16 @@ std::string InMemUserTask::taskStatusString() const
             return "Completed";
     }
 }
+
+void InMemUserTask::setCreationDate()
+{
+    std::time_t currentTime = std::time(0);
+    std::tm* now = std::localtime(&currentTime);
+
+    int year = now->tm_year + 1900;
+    int month = now->tm_mon + 1;
+    int day = now->tm_mday;
+
+    createdOnDate = std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day);
+}
+
--- Development/Prototypes/CPPDataBase/InMemUserTask.h
+++ Development/Prototypes/CPPDataBase/InMemUserTask.h
@@ -1,6 +1,7 @@
 #ifndef INMEMUSERTASK_H_
 #define INMEMUSERTASK_H_
 
+#include "InMemUser.h"
 #include <iostream>
 #include <string>
 
@@ -9,22 +10,57 @@ class InMemUserTask
 public:
     enum class TaskStatus
     {
-        Not_Started, On_Hold, Waiting_for_Dependency, Work_in_Progress, Complete
+        Not_Started=1, On_Hold=2, Waiting_for_Dependency=3, Work_in_Progress=4, Complete=5
     };
 
     InMemUserTask();
+    // new task from GUI or program.
+    InMemUserTask(InMemUser& creator, std::string& descriptionIn, unsigned int estimatedHoursEffort, std::string dueDate,
+        std::string startDate, InMemUserTask* parentTaskp = nullptr, TaskStatus statusIn = TaskStatus::Not_Started,
+        unsigned int majorPriority = 1, unsigned int minorPriority = 1
+    );
+    // existing task from database.
+    InMemUserTask(std::size_t taskIDfromDB, std::size_t createdByUidfromDb, std::size_t assignedToUidfromDb,
+        std::string descriptionfromDb, std::size_t parentTaskIDfromDb, unsigned int statusfromDb,
+        double percentageCompletefromDb, std::string createdOnDatefromDb, std::string requiredDeliveryDatefromDb,
+        std::string scheduledStartDatefromDb, std::string actualStartDatefromDb, std::string estimatedCompletionDatefromDb,
+        std::string completedDatefromDb, unsigned int estimatedEffortHoursfromDb, double actualEffortHoursfromDb,
+        unsigned int priorityGroupfromDb, unsigned int priorityfromDb      
+    );
     ~InMemUserTask();
     std::string taskStatusString() const;
 
     friend std::ostream& operator<<(std::ostream& os, const InMemUserTask& obj)
     {
-        os << "User Task\n";
-        os << "\tDatabase Task ID" << obj.taskID;
+        os << "User Task:\n";
+        os << "\tDatabase Task ID:\t" << obj.taskID << "\n";
+        os << "\tCreated By User ID:\t" << obj.createdByUid << "\n";
+        os << "\tAssign to User ID:\t" << obj.assignedToUid << "\n";
+        os << "\tDescription:\t" << obj.description << "\n";
+        os << "\tStatus integer value:\t" << static_cast<int>(obj.status) << "\n";
+        os << "\tParent Task ID:\t" << obj.parentTaskID << "\n";
+        os << "\tPercentage Complete:\t" << obj.percentageComplete << "%\n";
+        os << "\tCreated on date:\t" << obj.createdOnDate << "\n";
+        os << "\tRequired Deliverly date:\t" << obj.requiredDeliveryDate << "\n";
+        os << "\tScheduled Start date:\t" << obj.scheduledStartDate << "\n";
+        os << "\tActual Start date:\t" << obj.actualStartDate << "\n";
+        os << "\tEstimated Completion date:\t" << obj.estimatedCompletionDate << "\n";
+        os << "\tCompleted date:\t" << obj.completedDate << "\n";
+        os << "\tEstimated Effort in hours:\t" << obj.estimatedEffortHours << "\n";
+        os << "\tActual Effort Hours to date:\t" << obj.actualEffortHours << "\n";
+        os << "\tPriority Group:\t" << obj.priorityGroup << "\n";
+        os << "\tPriority:\t" << obj.priority << "%\n";
         return os;
     };
 
 
 private:
+    void setCreationDate();
+    TaskStatus statusFromInt(unsigned int statusI) const { return static_cast<InMemUserTask::TaskStatus>(statusI); };
+
+/*
+ * Member variables.
+ */
     std::size_t taskID;
     std::size_t createdByUid;
     std::size_t assignedToUid;
