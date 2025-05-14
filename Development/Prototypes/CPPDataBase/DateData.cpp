b63998ae34fbcc81a068a175321d7de8ecac6d34
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-14 08:26:26 -0700

    Fully Implemented DateData

--- Development/Prototypes/CPPDataBase/DateData.cpp
+++ Development/Prototypes/CPPDataBase/DateData.cpp
@@ -1,44 +1,66 @@
 #include "DateData.h"
+#include <ctime>
+#include <iomanip>
 #include <iostream>
-
-DateData::DateData()
-: year{0}, month{0}, day{0}
-{
-}
+#include <locale>
+#include <sstream>
+#include <vector>
 
 DateData::DateData(std::string dateString)
 : year{0}, month{0}, day{0}
 {
-    std::cerr << "\n\nDateData::DateData(" << dateString << ") NOT IMPLEMENTED!!!\n\n";
+    std::locale usEnglish("en_US.UTF-8");
+    std::vector<std::string> legalFormats = {
+        {"%B %d, %Y"},
+        {"%m/%d/%Y"},
+        {"%m-%d-%Y"},
+        {"%Y-%m-%d"}
+    };
+
+    for (auto legalFormat: legalFormats)
+    {
+        std::tm dateFromString{};
+        std::istringstream ss(dateString);
+        ss.imbue(usEnglish);
+        ss >> std::get_time(&dateFromString, legalFormat.c_str());
+        if (!ss.fail())
+        {
+            translateFromStdTM(dateFromString);
+            return;
+        }
+    }
+
 }
 
 DateData::DateData(std::time_t currentTime)
 {
     std::tm* now = std::localtime(&currentTime);
-
-    year = now->tm_year + 1900;
-    month = now->tm_mon + 1;
-    day = now->tm_mday;
+    translateFromStdTM(*now);
 }
 
-std::string DateData::getDBDateValue()
+void DateData::translateFromStdTM(std::tm someDate)
 {
-    if (year == 0 || month == 0 || day == 0)
-    {
-        std::string errString = "\n\nDate Not Set!!!\n\n";
-        std::cerr << errString;
-        return errString;
-    }
-    else
-    {
-        return std::to_string(year) + "-" + noSingleDigitInDate(month) +
-            "-" + noSingleDigitInDate(day);
-    }
+    year = someDate.tm_year + 1900;
+    month = someDate.tm_mon + 1;
+    day = someDate.tm_mday;
 }
 
-std::string DateData::noSingleDigitInDate(unsigned int valueToConvert)
+std::tm DateData::translateToStdTM()
 {
-    return valueToConvert > 10? std::to_string(valueToConvert) :
-        "0" + std::to_string(valueToConvert);
+    std::tm LinuxDate{};
+    LinuxDate.tm_year = year - 1900;
+    LinuxDate.tm_mon = month -1;
+    LinuxDate.tm_mday = day;
+    return LinuxDate;
 }
 
+std::string DateData::USEnglishDateUsingFormat(const char *format)
+{
+    std::locale usEnglish("en_US.UTF-8");
+    std::tm dateConverter = translateToStdTM();
+    std::stringstream ss;
+    ss.imbue(usEnglish);
+    ss << std::put_time(&dateConverter, format);
+
+    return ss.str();
+}
