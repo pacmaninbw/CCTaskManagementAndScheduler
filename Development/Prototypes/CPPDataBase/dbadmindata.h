6d26217e6d46dbd4e042f8aac9c5e21da5626182
pacmaninbw <paul.chernick@chernicksw.com>
2025-05-31 09:25:58 -0700

    added dbadmindata.h, the public version has no private information, the private version is in a directory that is not under GIT version control so that the actual connection data remains private.

---
+++ Development/Prototypes/CPPDataBase/dbadmindata.h
@@ -0,0 +1,12 @@
+#ifndef DBADMINDATA_H_
+#define DBADMINDATA_H_
+
+#include <string>
+
+static const std::string MySQLAdminUser{"REDACTED"};
+static const std::string MySQLAdminPassword{"REDACTED"};
+static const std::string PlannerDB{"PlannerTaskScheduleDB"};
+
+#endif // DBADMINDATA_H_
+
+
