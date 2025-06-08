9685cf884cd24e2a75f05f3ec640ff58a00de823
pacmaninbw <paul.chernick@chernicksw.com>
2025-06-08 05:35:54 -0700

    Added data field access functionality.

--- Development/Prototypes/CPPDataBase/ModelBase.cpp
+++ Development/Prototypes/CPPDataBase/ModelBase.cpp
@@ -1,15 +1,14 @@
 #include "ModelBase.h"
 
-ModelBase::ModelBase(std::size_t fieldCount, std::size_t primaryKeyIn)
-: numberOfFields{fieldCount},
-  fieldsHaveBeenUpdated{0},
-  updatedFieldsMask{0},
-  primaryKey{primaryKeyIn}
+ModelBase::ModelBase(std::string primaryKeyName, std::size_t primaryKeyIn)
+: primaryKeyFieldName{primaryKeyName}
 {
-    for (std::size_t i = 0; i < fieldCount; ++i)
+    PTS_DataField* primaryKey = new PTS_DataField(PTS_DataField::PTS_DB_FieldType::Key, primaryKeyName, true);
+    if (primaryKeyIn)
     {
-        updatedFieldsMask |= 1 << i;
+        primaryKey->setValue(primaryKeyIn);
     }
+    dataFields.insert({primaryKeyName, primaryKey});
 }
 
 ModelBase::~ModelBase()
@@ -17,4 +16,108 @@ ModelBase::~ModelBase()
 
 }
 
+void ModelBase::addDataField(std::string& fieldName, PTS_DataField::PTS_DB_FieldType fieldType, bool required)
+{
+    PTS_DataField* dataField = new PTS_DataField(fieldType, fieldName, required);
+    dataFields.insert({fieldName, dataField});
+}
 
+bool ModelBase::setFieldValue(std::string fieldName, DataValueType dataValue)
+{
+    auto fieldToUpdate = dataFields.find(fieldName);
+    if (fieldToUpdate != dataFields.end())
+    {
+        fieldToUpdate->second->setValue(dataValue);
+        return true;
+    }
+    else
+    {
+        std::cerr << "Field not found in dataFields: " << fieldName << "\n";
+        return false;
+    }
+}
+
+std::string ModelBase::getFieldValueString(std::string fieldName)
+{
+    std::string valueString("No Value Set");
+
+    auto fieldToFind = dataFields.find(fieldName);
+    if (fieldToFind != dataFields.end())
+    {
+        if (fieldToFind->second->hasValue())
+        {
+            valueString = fieldToFind->second->toString();
+        }
+    }
+    else
+    {
+        std::cerr << "Field not found in dataFields: " << fieldName << "\n";
+    }
+
+    return valueString;
+}
+
+void ModelBase::setPrimaryKey(std::size_t keyValue)
+{
+    if (!setFieldValue(primaryKeyFieldName, keyValue))
+    {
+        std::cerr << "Internal Error, primary key not found in dataFields\n";
+    }
+}
+
+std::size_t ModelBase::getPrimaryKey() const
+{
+    std::size_t primaryKey = 0;
+
+    auto primaryKeyField = dataFields.find(primaryKeyFieldName);
+    if (primaryKeyField != dataFields.end())
+    {
+        if (primaryKeyField->second->hasValue())
+        {
+            DataValueType dataValue = primaryKeyField->second->getValue();
+            primaryKey = std::get<std::size_t>(dataValue);
+        }
+    }
+    else
+    {
+        std::cerr << "Internal Error, primary key not found in dataFields\n";
+    }
+
+    return primaryKey;
+}
+
+DataValueType ModelBase::getFieldValue(std::string fieldName)
+{
+    DataValueType dataValue;
+    auto fieldToFind = dataFields.find(fieldName);
+    if (fieldToFind != dataFields.end())
+    {
+        if (fieldToFind->second->hasValue())
+        {
+            dataValue = fieldToFind->second->getValue();
+        }
+    }
+    else
+    {
+        std::cerr << "Field not found in dataFields: " << fieldName << "\n";
+    }
+
+    return dataValue;
+}
+
+bool ModelBase::fieldHasValue(std::string fieldName)
+{
+    bool hasValue = false;
+
+    auto fieldToFind = dataFields.find(fieldName);
+    if (fieldToFind != dataFields.end())
+    {
+        hasValue = fieldToFind->second->hasValue();
+    }
+    else
+    {
+        std::cerr << "Field not found in dataFields: " << fieldName << "\n";
+    }
+
+    return hasValue;
+}
--- Development/Prototypes/CPPDataBase/ModelBase.h
+++ Development/Prototypes/CPPDataBase/ModelBase.h
@@ -1,22 +1,37 @@
 #ifndef MODELBASE_H_
 #define MODELBASE_H_
 
-#include <cstddef>
 #include "PTS_DataField.h"
+#include <string>
+#include <unordered_map>
 
 class ModelBase
 {
 public:
-    ModelBase(std::size_t fieldCount, std::size_t primaryKeyIn=0);
+    ModelBase(std::string primaryKeyName, std::size_t primaryKeyIn=0);
     virtual ~ModelBase();
-    virtual void setPrimaryKey(std::size_t keyValue) { primaryKey = keyValue; };
-    virtual std::size_t getPrimaryKey() const { return primaryKey; };
+    void setPrimaryKey(std::size_t keyValue);
+    std::size_t getPrimaryKey() const;
+    void addDataField(std::string& fieldName, PTS_DataField::PTS_DB_FieldType fieldType, bool required=false);
+    [[nodiscard]] bool setFieldValue(std::string fieldName, DataValueType dataValue);
+    std::string getFieldValueString(std::string fieldName);
+    DataValueType getFieldValue(std::string fieldName);
+    bool fieldHasValue(std::string fieldName);
+
+    friend std::ostream& operator<<(std::ostream& os, const ModelBase& obj)
+    {
+        os << "Primary Key field name :" << obj.primaryKeyFieldName << "\n";
+        for (const auto& [key, value] : obj.dataFields)
+        {
+            PTS_DataField* currentField = value;
+            os << currentField->fieldInfo();
+        }
+        return os;
+    };
 
 protected:
-    std::size_t numberOfFields;
-    std::size_t fieldsHaveBeenUpdated;
-    std::size_t updatedFieldsMask;
-    std::size_t primaryKey;
+    std::string primaryKeyFieldName;
+    std::unordered_map<std::string, PTS_DataField*> dataFields;
 };
 
 #endif  // MODELBASE_H_
