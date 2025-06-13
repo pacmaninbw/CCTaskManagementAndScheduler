5bc4dfc631dbbf0804e4410a350ab8f4aaddb36d
pacmaninbw <paul.chernick@chernicksw.com>
2025-06-13 07:47:29 -0700

    Converted user preferences for start time and end time to string. std::chrono::time_point is not a good match for times without a date.

--- Development/Prototypes/CPPDataBase/UserModel.cpp
+++ Development/Prototypes/CPPDataBase/UserModel.cpp
@@ -12,8 +12,8 @@ UserModel::UserModel()
     addDataField("LoginName",  PTS_DataField::PTS_DB_FieldType::VarChar45, true);
     addDataField("HashedPassWord", PTS_DataField::PTS_DB_FieldType::TinyText, true);
     addDataField("emailAddress", PTS_DataField::PTS_DB_FieldType::VarChar256, true);
-    addDataField("ScheduleDayStart", PTS_DataField::PTS_DB_FieldType::DateTime, true);
-    addDataField("ScheduleDayEnd", PTS_DataField::PTS_DB_FieldType::DateTime, true);
+    addDataField("ScheduleDayStart", PTS_DataField::PTS_DB_FieldType::VarChar45, true);
+    addDataField("ScheduleDayEnd", PTS_DataField::PTS_DB_FieldType::VarChar45, true);
     addDataField("IncludePriorityInSchedule", PTS_DataField::PTS_DB_FieldType::Boolean);
     addDataField("IncludeMinorPriorityInSchedule", PTS_DataField::PTS_DB_FieldType::Boolean);
     addDataField("UseLettersForMajorPriority", PTS_DataField::PTS_DB_FieldType::Boolean);
@@ -22,10 +22,6 @@ UserModel::UserModel()
 
 UserModel::UserModel(const char *lastIn, const char *firstIn, const char *middleIIn, const char *email)
     : UserModel()
-    /*,
-    scheduleDayStart{"8:30 AM"},
-    scheduleDayEnd{"5:00 PM"},
-    */
 {
     setFieldValue("LastName", lastIn);
     setFieldValue("FirstName", firstIn);
@@ -36,6 +32,8 @@ UserModel::UserModel(const char *lastIn, const char *firstIn, const char *middle
     initFieldValueNotChanged("IncludeMinorPriorityInSchedule", true);
     initFieldValueNotChanged("UseLettersForMajorPriority", true);
     initFieldValueNotChanged("SeparatePriorityWithDot", false);
+    initFieldValueNotChanged("ScheduleDayStart", "8:30 AM");
+    initFieldValueNotChanged("ScheduleDayEnd", "5:00 PM");
 }
 
 UserModel::UserModel(std::size_t dbUserId, const char *dbLoginName, const char *dbPassWord, const char *dbLastName,
@@ -55,52 +53,13 @@ UserModel::UserModel(std::size_t dbUserId, const char *dbLoginName, const char *
     initFieldValueNotChanged("IncludeMinorPriorityInSchedule", dbMinorPriorityInSched != 0);
     initFieldValueNotChanged("UseLettersForMajorPriority", dbLettersForMajorP != 0);
     initFieldValueNotChanged("SeparatePriorityWithDot", dbUseDot != 0);
-    std::cout << "Start Day" << dbDayStart << "\n";
-    std::cout << "End Day" << dbDayEnd << "\n";
+    initFieldValueNotChanged("ScheduleDayStart", dbDayStart);
+    initFieldValueNotChanged("ScheduleDayEnd", dbDayEnd);
 }
 
 UserModel::~UserModel()
 {
 
-}
-
-std::string UserModel::convertTo24hours(std::string sechduledTime)
-{
-    std::string original(sechduledTime);
-
-    std::size_t colonIdx = original.find(':');
-    if (colonIdx == std::string::npos) {
-
-        std::runtime_error noColon("Colon not found between hours and minutes in UserModel::convertTo24hours()");
-        throw noColon;
-    }
-
-    std::size_t spaceIdx = original.find(' ');
-    if (spaceIdx == std::string::npos) {
-
-        std::runtime_error noSpace("Space not found after minutes in UserModel::convertTo24hours()");
-        throw noSpace;
-    }
-
-    std::string hourSubStr = original.substr(0, colonIdx - 1); // May be 1 or 2 digits.
-    std::string minuteStr = original.substr(colonIdx + 1, spaceIdx - (colonIdx + 1));
-    std::string ampm = original.substr(spaceIdx + 1, 2);
-
-    int startHour = std::stoi(hourSubStr);
-    
-    std::string newTime;
-    if (ampm[0] == 'A')
-    {
-        newTime = "0" + std::to_string(startHour);
-    }
-    else
-    {
-        newTime = std::to_string(startHour + 12);
-    }
-
-    newTime += ":" + minuteStr;
-
-    return static_cast<std::string>(newTime);
 }
 
 void UserModel::createLoginBasedOnUserName(std::string lastName, std::string firstName, std::string middleInitial)
