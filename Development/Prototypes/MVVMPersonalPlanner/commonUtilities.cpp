481f0f149599078aa763d92ff4ecbff12b31ee5a
pacmaninbw <paul.chernick@chernicksw.com>
2025-08-03 08:17:25 -0700

    Added 2 utility functions to add or subtract days from today.

--- Development/Prototypes/MVVMPersonalPlanner/commonUtilities.cpp
+++ Development/Prototypes/MVVMPersonalPlanner/commonUtilities.cpp
@@ -7,3 +7,16 @@ std::chrono::year_month_day getTodaysDate()
     return std::chrono::floor<std::chrono::days>(today);
 }
 
+std::chrono::year_month_day getTodaysDatePlus(unsigned int offset)
+{
+    std::chrono::time_point<std::chrono::system_clock> futureDate = std::chrono::system_clock::now();
+    futureDate += std::chrono::days(offset);
+    return std::chrono::floor<std::chrono::days>(futureDate);
+}
+
+std::chrono::year_month_day getTodaysDateMinus(unsigned int offset)
+{
+    std::chrono::time_point<std::chrono::system_clock> pastDate = std::chrono::system_clock::now();
+    pastDate -= std::chrono::days(offset);
+    return std::chrono::floor<std::chrono::days>(pastDate);
+}
--- Development/Prototypes/MVVMPersonalPlanner/commonUtilities.h
+++ Development/Prototypes/MVVMPersonalPlanner/commonUtilities.h
@@ -3,5 +3,7 @@
 #include <chrono>
 
 extern std::chrono::year_month_day getTodaysDate();
+extern std::chrono::year_month_day getTodaysDatePlus(unsigned int offset);
+extern std::chrono::year_month_day getTodaysDateMinus(unsigned int offset);
 
 #endif // COMMONUTILITIES_H_
