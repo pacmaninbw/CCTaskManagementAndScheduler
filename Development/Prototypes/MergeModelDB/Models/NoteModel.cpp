f5f52bcb428b54825046626d36cb917d9ed8306a
pacmaninbw <paul.chernick@chernicksw.com>
2025-09-19 13:54:35 -0700

    Implemented the Note model class. Still need to implement note lists and note tests.

---
+++ Development/Prototypes/MergeModelDB/Models/NoteModel.cpp
@@ -0,0 +1,135 @@
+#include <exception>
+#include <chrono>
+#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include "NoteModel.h"
+#include <vector>
+
+NoteModel::NoteModel()
+: ModelDBInterface("Note")
+{
+    userID = 0;
+    creationDate = std::chrono::system_clock::now();
+    lastUpdate = creationDate;
+}
+
+void NoteModel::setNoteId(std::size_t noteId)
+{
+    modified = true;
+    primaryKey = noteId;
+}
+
+void NoteModel::setUserId(std::size_t userId)
+{
+    modified = true;
+    userID = userId;
+}
+
+void NoteModel::setContent(std::string contentStr)
+{
+    modified = true;
+    content = contentStr;
+}
+
+void NoteModel::setDateAdded(std::chrono::system_clock::time_point created)
+{
+    modified = true;
+    creationDate = created;
+}
+
+void NoteModel::setLastModified(std::chrono::system_clock::time_point lastModification)
+{
+    modified = true;
+    lastUpdate = lastModification;
+}
+
+bool NoteModel::diffUser(NoteModel &other)
+{
+    // Ignore user preferences
+    return (primaryKey == other.primaryKey && userID == other.userID && content == other.content &&
+        creationDate == other.creationDate && lastUpdate == other.lastUpdate);
+}
+
+void NoteModel::initRequiredFields()
+{
+    missingRequiredFieldsTests.push_back({std::bind(&NoteModel::isMissingUserID, this), "User ID"});
+    missingRequiredFieldsTests.push_back({std::bind(&NoteModel::isMissingContent, this), "Content"});
+    missingRequiredFieldsTests.push_back({std::bind(&NoteModel::isMissingCreationDate, this), "Date Created"});
+    missingRequiredFieldsTests.push_back({std::bind(&NoteModel::isMissingLastUpdate, this), "Password"});
+}
+
+std::string NoteModel::formatInsertStatement()
+{
+    initFormatOptions();
+
+    std::string insertStatement = NSBM::format_sql(format_opts.value(),
+        "INSERT INTO UserNotes (UserID, NotationDateTime, Content, LastUpdate) VALUES ({0}, {1}, {2}, {3})",
+        userID, stdChronoTimePointToBoostDateTime(creationDate), content, stdChronoTimePointToBoostDateTime(lastUpdate));
+
+    return insertStatement;
+}
+
+std::string NoteModel::formatUpdateStatement()
+{
+    initFormatOptions();
+
+    std::string updateStatement = NSBM::format_sql(format_opts.value(),
+        "UPDATE UserProfile SET"
+            " UserNotes.UserID = {0},"
+            " UserNotes.NotationDateTime = {1},"
+            " UserNotes.Content = {2},"
+            " UserNotes.LastUpdate = {3}" 
+        " WHERE UserNotes.idUserNotes = {4}",
+            userID, stdChronoTimePointToBoostDateTime(creationDate), content, stdChronoTimePointToBoostDateTime(lastUpdate));
+        
+    return updateStatement;
+}
+
+std::string NoteModel::formatSelectStatement()
+{
+    initFormatOptions();
+
+    NSBM::format_context fctx(format_opts.value());
+    NSBM::format_sql_to(fctx, baseQuery);
+    NSBM::format_sql_to(fctx, " WHERE UserID = {}", primaryKey);
+
+    return std::move(fctx).get().value();
+}
+
+
+void NoteModel::processResultRow(NSBM::row_view rv)
+{
+    primaryKey = rv.at(NoteIdIdx).as_uint64();
+    userID = rv.at(UserIdIdx).as_uint64();
+    creationDate = boostMysqlDateTimeToChronoTimePoint(rv.at(NotationDateTimeIdx).as_datetime());
+    content = rv.at(ContentIdx).as_string();
+    lastUpdate = boostMysqlDateTimeToChronoTimePoint(rv.at(LastUpdateIdx).as_datetime());
+}
+
+bool NoteModel::selectByNoteID(std::size_t noteID)
+{
+    prepareForRunQueryAsync();
+
+    try
+    {
+        NSBM::format_context fctx(format_opts.value());
+        NSBM::format_sql_to(fctx, baseQuery);
+        NSBM::format_sql_to(fctx, " WHERE idUserNotes = {}", noteID);
+
+        NSBM::results localResult = runQueryAsync(std::move(fctx).get().value());
+
+        return processResult(localResult);
+    }
+
+    catch(const std::exception& e)
+    {
+        appendErrorMessage(std::format("In NoteModel::selectByUserID : {}", e.what()));
+        return false;
+    }
+}
---
+++ Development/Prototypes/MergeModelDB/Models/NoteModel.h
@@ -0,0 +1,102 @@
+#ifndef NOTEMODEL_H_
+#define NOTEMODEL_H_
+
+#include <chrono>
+#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include "ModelDBInterface.h"
+#include <optional>
+#include <string>
+#include <vector>
+
+class NoteModel : public ModelDBInterface
+{
+public:
+
+    NoteModel();
+    ~NoteModel() = default;
+
+    std::size_t getNoteId() const { return primaryKey; };
+    std::size_t getUserId() const { return userID; };
+    std::string getContent() const { return content; };
+    std::chrono::system_clock::time_point getDateAdded() const { return creationDate; };
+    std::chrono::system_clock::time_point getLastModified() const { return lastUpdate; };
+
+    void setNoteId(std::size_t noteId);
+    void setUserId(std::size_t userId);
+    void setContent(std::string contentStr);
+    void setDateAdded(std::chrono::system_clock::time_point created);
+    void setLastModified(std::chrono::system_clock::time_point lastModification);
+/*
+ * Select with arguments
+ */
+    bool selectByNoteID(std::size_t noteID);
+
+/*
+ * Required fields.
+ */
+    bool isMissingUserID()  { return userID == 0; };;
+    bool isMissingContent() { return (content.empty() || content.size() < 10); };
+    bool isMissingCreationDate() { return creationDate.time_since_epoch() != std::chrono::system_clock::duration::zero(); };
+    bool isMissingLastUpdate() { return lastUpdate.time_since_epoch() != std::chrono::system_clock::duration::zero(); };
+    void initRequiredFields() override;
+
+    bool operator==(NoteModel& other)
+    {
+        return diffUser(other);
+    };
+    bool operator==(std::shared_ptr<NoteModel> other)
+    {
+        return diffUser(*other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const NoteModel& note)
+    {
+        constexpr const char* outFmtStr = "\t{}: {}\n";
+        os << std::format(outFmtStr, "Note ID", note.primaryKey);
+        os << std::format(outFmtStr, "User ID", note.userID);
+        os << std::format(outFmtStr, "Content", note.content);
+        os << std::format(outFmtStr, "Created", note.creationDate);
+        os << std::format(outFmtStr, "Last Update", note.lastUpdate);
+
+        return os;
+    };
+
+private:
+    bool diffUser(NoteModel& other);
+    std::string formatInsertStatement() override;
+    std::string formatUpdateStatement() override;
+    std::string formatSelectStatement() override;
+
+    std::string buildPreferenceText() noexcept;
+    void parsePrefenceText(std::string preferences) noexcept;
+    void processResultRow(NSBM::row_view rv) override;
+    
+    std::size_t userID;
+    std::string content;
+    std::chrono::system_clock::time_point creationDate;
+    std::chrono::system_clock::time_point lastUpdate;
+
+
+private:
+/*
+ * The indexes below are based on the following select statement, maintain this order.
+ * baseQuery could be SELECT * FROM UserProfile, but this way the order of the columns
+ * returned are known.
+ */
+    NSBM::constant_string_view baseQuery = 
+        "SELECT idUserNotes, UserID, NotationDateTime, Content, LastUpdate FROM UserNotes ";
+
+    const std::size_t NoteIdIdx = 0;
+    const std::size_t UserIdIdx = 1;
+    const std::size_t NotationDateTimeIdx = 2;
+    const std::size_t ContentIdx = 3;
+    const std::size_t LastUpdateIdx = 4;
+};
+
+using NoteModel_shp = std::shared_ptr<NoteModel>;
+
+#endif // NOTEMODEL_H_
