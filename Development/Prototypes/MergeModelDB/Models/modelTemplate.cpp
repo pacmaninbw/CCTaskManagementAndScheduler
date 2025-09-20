96e2622037040585eff34af14810b82aecdfc7b6
pacmaninbw <paul.chernick@chernicksw.com>
2025-09-20 05:38:08 -0700

    Created modelTemplate.* as fast starters for the creation of new models.

---
+++ Development/Prototypes/MergeModelDB/Models/modelTemplate.cpp
@@ -0,0 +1,108 @@
+#include <exception>
+#include <chrono>
+//#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include "modelTemplate.h"
+#include <vector>
+
+TemplateModel::TemplateModel()
+: ModelDBInterface("TemplateModel")
+{
+    userID = 0;
+    creationDate = std::chrono::system_clock::now();
+    lastUpdate = creationDate;
+}
+
+void TemplateModel::setNoteId(std::size_t noteId)
+{
+    modified = true;
+    primaryKey = noteId;
+}
+
+
+bool TemplateModel::diffUser(TemplateModel &other)
+{
+    // Ignore user preferences
+    return (primaryKey == other.primaryKey && userID == other.userID);
+}
+
+void TemplateModel::initRequiredFields()
+{
+    missingRequiredFieldsTests.push_back({std::bind(&TemplateModel::isMissingUserID, this), "User ID"});
+}
+
+std::string TemplateModel::formatInsertStatement()
+{
+    initFormatOptions();
+
+    std::string insertStatement = NSBM::format_sql(format_opts.value(),
+        "INSERT INTO UserNotes (UserID, NotationDateTime, Content, LastUpdate) VALUES ({0}, {1}, {2}, {3})",
+        userID, stdChronoTimePointToBoostDateTime(creationDate),
+        content, stdChronoTimePointToBoostDateTime(lastUpdate));
+
+    return insertStatement;
+}
+
+std::string TemplateModel::formatUpdateStatement()
+{
+    initFormatOptions();
+
+    std::string updateStatement = NSBM::format_sql(format_opts.value(),
+        "UPDATE UserProfile SET"
+            " UserNotes.UserID = {0},"
+            " UserNotes.LastUpdate = {3}" 
+        " WHERE UserNotes.idUserNotes = {4}",
+            userID, stdChronoTimePointToBoostDateTime(creationDate),
+            content, stdChronoTimePointToBoostDateTime(lastUpdate));
+        
+    return updateStatement;
+}
+
+std::string TemplateModel::formatSelectStatement()
+{
+    initFormatOptions();
+
+    NSBM::format_context fctx(format_opts.value());
+    NSBM::format_sql_to(fctx, baseQuery);
+    NSBM::format_sql_to(fctx, " WHERE UserID = {}", primaryKey);
+
+    return std::move(fctx).get().value();
+}
+
+
+void TemplateModel::processResultRow(NSBM::row_view rv)
+{
+    primaryKey = rv.at(NoteIdIdx).as_uint64();
+    userID = rv.at(UserIdIdx).as_uint64();
+    creationDate = boostMysqlDateTimeToChronoTimePoint(rv.at(NotationDateTimeIdx).as_datetime());
+    content = rv.at(ContentIdx).as_string();
+    lastUpdate = boostMysqlDateTimeToChronoTimePoint(rv.at(LastUpdateIdx).as_datetime());
+}
+
+bool TemplateModel::selectByNoteID(std::size_t noteID)
+{
+    prepareForRunQueryAsync();
+
+    try
+    {
+        NSBM::format_context fctx(format_opts.value());
+        NSBM::format_sql_to(fctx, baseQuery);
+        NSBM::format_sql_to(fctx, " WHERE idUserNotes = {}", noteID);
+
+        NSBM::results localResult = runQueryAsync(std::move(fctx).get().value());
+
+        return processResult(localResult);
+    }
+
+    catch(const std::exception& e)
+    {
+        appendErrorMessage(std::format("In TemplateModel::selectByUserID : {}", e.what()));
+        return false;
+    }
+}
---
+++ Development/Prototypes/MergeModelDB/Models/modelTemplate.h
@@ -0,0 +1,87 @@
+#ifndef MODELTEMPLACE_H_
+#define MODELTEMPLACE_H_
+
+#include <chrono>
+//#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include "ModelDBInterface.h"
+#include <optional>
+#include <string>
+#include <vector>
+
+class TemplateModel : public ModelDBInterface
+{
+public:
+
+    TemplateModel();
+    ~TemplateModel() = default;
+
+// get access methods
+    std::size_t getNoteId() const { return primaryKey; };
+
+// set access methods
+    void setNoteId(std::size_t noteId);
+/*
+ * Select with arguments
+ */
+    bool selectByNoteID(std::size_t noteID);
+
+/*
+ * Required fields.
+ */
+    bool isMissingUserID()  { return userID == 0; };
+    void initRequiredFields() override;
+
+    bool operator==(TemplateModel& other)
+    {
+        return diffUser(other);
+    };
+    bool operator==(std::shared_ptr<TemplateModel> other)
+    {
+        return diffUser(*other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const TemplateModel& note)
+    {
+        constexpr const char* outFmtStr = "\t{}: {}\n";
+        os << std::format(outFmtStr, "Note ID", note.primaryKey);
+
+        return os;
+    };
+
+private:
+    bool diffUser(TemplateModel& other);
+    void processResultRow(NSBM::row_view rv) override;
+    std::string formatInsertStatement() override;
+    std::string formatUpdateStatement() override;
+    std::string formatSelectStatement() override;
+
+    
+    std::size_t userID;
+    std::string content;
+    std::chrono::system_clock::time_point creationDate;
+    std::chrono::system_clock::time_point lastUpdate;
+
+
+private:
+/*
+ * The indexes below are based on the following select statement, maintain this order.
+ * baseQuery could be SELECT * FROM UserProfile, but this way the order of the columns
+ * returned are known.
+ */
+    NSBM::constant_string_view baseQuery = 
+        "SELECT idUserNotes, UserID, NotationDateTime, Content, LastUpdate FROM UserNotes ";
+
+    const std::size_t NoteIdIdx = 0;
+    const std::size_t UserIdIdx = 1;
+    const std::size_t NotationDateTimeIdx = 2;
+    const std::size_t ContentIdx = 3;
+    const std::size_t LastUpdateIdx = 4;
+};
+
+using TemplateModel_shp = std::shared_ptr<TemplateModel>;
+
+#endif // MODELTEMPLACE_H_
