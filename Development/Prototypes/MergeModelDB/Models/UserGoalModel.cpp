b22c85c42e4c3ca63b8c887813aaabcc83f7eb16
pacmaninbw <paul.chernick@chernicksw.com>
2025-09-21 07:16:04 -0700

    Created a model for user goals.

---
+++ Development/Prototypes/MergeModelDB/Models/UserGoalModel.cpp
@@ -0,0 +1,156 @@
+#include <exception>
+#include <chrono>
+//#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include "UserGoalModel.h"
+#include <vector>
+
+UserGoalModel::UserGoalModel()
+: ModelDBInterface("UserGoalModel")
+{
+    userID = 0;
+    creationDate = std::chrono::system_clock::now();
+    lastUpdate = creationDate;
+}
+
+void UserGoalModel::setGoalId(std::size_t userGoalId)
+{
+    modified = true;
+    primaryKey = userGoalId;
+}
+
+void UserGoalModel::setDescription(std::string newDescription)
+{
+    modified = true;
+    description = newDescription;
+}
+
+void UserGoalModel::setPriority(unsigned int newPriority)
+{
+    modified = true;
+    priority = newPriority;
+}
+
+void UserGoalModel::setParentID(std::size_t newParentID)
+{
+    modified = true;
+    parentID = newParentID;
+}
+
+void UserGoalModel::setCreationTimeStamp(std::chrono::system_clock::time_point newCreationTS)
+{
+    modified = true;
+    creationDate = newCreationTS;
+}
+
+void UserGoalModel::setLastUpdateTimeStamp(std::chrono::system_clock::time_point newLastUpdateTS)
+{
+    modified = true;
+    lastUpdate = newLastUpdateTS;
+}
+
+bool UserGoalModel::diffGoal(UserGoalModel &other)
+{
+    return (primaryKey == other.primaryKey && userID == other.userID &&
+        description == other.description);
+}
+
+void UserGoalModel::initRequiredFields()
+{
+    missingRequiredFieldsTests.push_back({std::bind(&UserGoalModel::isMissingUserID, this), "User ID"});
+    missingRequiredFieldsTests.push_back({std::bind(&UserGoalModel::isMissingDescription, this), "Description"});
+    missingRequiredFieldsTests.push_back({std::bind(&UserGoalModel::isMissingCreationDate, this), "Creation Timestamp"});
+    missingRequiredFieldsTests.push_back({std::bind(&UserGoalModel::isMissingLastUpdate, this), "Last Update"});
+}
+
+std::string UserGoalModel::formatInsertStatement()
+{
+    initFormatOptions();
+
+    std::string insertStatement = NSBM::format_sql(format_opts.value(),
+        "INSERT INTO UserGoals (UserID, Description, CreationTS, LastUpdateTS, "
+            "Priority, ParentGoal) VALUES ({0}, {1}, {2}, {3}, {4}, {5})",
+        userID, description, stdChronoTimePointToBoostDateTime(creationDate),
+        stdChronoTimePointToBoostDateTime(lastUpdate), priority, parentID);
+
+    return insertStatement;
+}
+
+std::string UserGoalModel::formatUpdateStatement()
+{
+    initFormatOptions();
+
+    std::string updateStatement = NSBM::format_sql(format_opts.value(),
+        "UPDATE UserGoals SET"
+            " UserGoals.UserID = {0},"
+            " UserGoals.Description = {1},"
+            " UserGoals.CreationTS = {2},"
+            " UserGoals.LastUpdateTS = {3}" 
+            " UserGoals.Priority = {4}" 
+            " UserGoals.ParentGoal = {5}" 
+        " WHERE UserGoals.idUserGoals = {6}",
+        userID, description, stdChronoTimePointToBoostDateTime(creationDate),
+        stdChronoTimePointToBoostDateTime(lastUpdate), priority, parentID, primaryKey);
+        
+    return updateStatement;
+}
+
+std::string UserGoalModel::formatSelectStatement()
+{
+    initFormatOptions();
+
+    NSBM::format_context fctx(format_opts.value());
+    NSBM::format_sql_to(fctx, baseQuery);
+    NSBM::format_sql_to(fctx, " WHERE idUserGoals = {}", primaryKey);
+
+    return std::move(fctx).get().value();
+}
+
+
+void UserGoalModel::processResultRow(NSBM::row_view rv)
+{
+    primaryKey = rv.at(GoalIdIdx).as_uint64();
+    userID = rv.at(UserIdIdx).as_uint64();
+    creationDate = boostMysqlDateTimeToChronoTimePoint(rv.at(CreationTSIdx).as_datetime());
+    description = rv.at(DescriptionIdx).as_string();
+    lastUpdate = boostMysqlDateTimeToChronoTimePoint(rv.at(LastUpdateIdx).as_datetime());
+
+    // Optional fields.
+    if (!rv.at(PriorityIdx).is_null())
+    {
+        priority = rv.at(PriorityIdx).as_uint64();
+    }
+
+    if (!rv.at(ParentGoalIDIdx).is_null())
+    {
+        parentID = rv.at(ParentGoalIDIdx).as_uint64();
+    }
+}
+
+bool UserGoalModel::selectByNoteID(std::size_t noteID)
+{
+    prepareForRunQueryAsync();
+
+    try
+    {
+        NSBM::format_context fctx(format_opts.value());
+        NSBM::format_sql_to(fctx, baseQuery);
+        NSBM::format_sql_to(fctx, " WHERE idUserGoals = {}", noteID);
+
+        NSBM::results localResult = runQueryAsync(std::move(fctx).get().value());
+
+        return processResult(localResult);
+    }
+
+    catch(const std::exception& e)
+    {
+        appendErrorMessage(std::format("In UserGoalModel::selectByUserID : {}", e.what()));
+        return false;
+    }
+}
---
+++ Development/Prototypes/MergeModelDB/Models/UserGoalModel.h
@@ -0,0 +1,102 @@
+#ifndef USERGOALMODEL_H_
+#define USERGOALMODEL_H_
+
+#include <chrono>
+//#include "commonUtilities.h"
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include "ModelDBInterface.h"
+#include <optional>
+#include <string>
+#include <vector>
+
+class UserGoalModel : public ModelDBInterface
+{
+public:
+
+    UserGoalModel();
+    ~UserGoalModel() = default;
+
+// get access methods
+    std::size_t getGoalId() const noexcept { return primaryKey; };
+    std::string getDescription() const noexcept { return description; };
+    unsigned int getPriority() const noexcept { return priority.value_or(0); };
+    std::size_t getParentId() const noexcept { return parentID.value_or(0); };
+    std::chrono::system_clock::time_point getCreationTimeStamp() const noexcept { return creationDate; };
+    std::chrono::system_clock::time_point getLastUpdateTimeStamp() const noexcept { return lastUpdate; };
+
+// set access methods
+    void setGoalId(std::size_t userGoalId);
+    void setDescription(std::string newDescription);
+    void setPriority(unsigned int newPriority);
+    void setParentID(std::size_t newParentID);
+    void setCreationTimeStamp(std::chrono::system_clock::time_point newCreationTS);
+    void setLastUpdateTimeStamp(std::chrono::system_clock::time_point newLastUpdateTS);
+/*
+ * Select with arguments
+ */
+    bool selectByNoteID(std::size_t noteID);
+
+/*
+ * Required fields.
+ */
+    bool isMissingUserID()  { return userID == 0; };
+    bool isMissingDescription() { return (description.empty() || description.size() < 10); };
+    bool isMissingCreationDate() { return creationDate.time_since_epoch() != std::chrono::system_clock::duration::zero(); };
+    bool isMissingLastUpdate() { return lastUpdate.time_since_epoch() != std::chrono::system_clock::duration::zero(); };
+    void initRequiredFields() override;
+
+    bool operator==(UserGoalModel& other)
+    {
+        return diffGoal(other);
+    };
+    bool operator==(std::shared_ptr<UserGoalModel> other)
+    {
+        return diffGoal(*other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const UserGoalModel& note)
+    {
+        constexpr const char* outFmtStr = "\t{}: {}\n";
+        os << std::format(outFmtStr, "Note ID", note.primaryKey);
+
+        return os;
+    };
+
+private:
+    bool diffGoal(UserGoalModel& other);
+    void processResultRow(NSBM::row_view rv) override;
+    std::string formatInsertStatement() override;
+    std::string formatUpdateStatement() override;
+    std::string formatSelectStatement() override;
+
+    
+    std::size_t userID;
+    std::string description;
+    std::optional<unsigned int> priority;
+    std::optional<std::size_t> parentID;
+    std::chrono::system_clock::time_point creationDate;
+    std::chrono::system_clock::time_point lastUpdate;
+
+
+private:
+/*
+ * The indexes below are based on the following select statement, maintain this order.
+ */
+    NSBM::constant_string_view baseQuery = 
+        "SELECT idUserGoals, UserID, Description, CreationTS, LastUpdateTS, Priority, ParentGoal FROM UserGoals ";
+
+    const std::size_t GoalIdIdx = 0;
+    const std::size_t UserIdIdx = 1;
+    const std::size_t DescriptionIdx = 2;
+    const std::size_t CreationTSIdx = 3;
+    const std::size_t LastUpdateIdx = 4;
+    const std::size_t PriorityIdx = 5;
+    const std::size_t ParentGoalIDIdx = 6;
+};
+
+using UserGoalModel_shp = std::shared_ptr<UserGoalModel>;
+
+#endif // USERGOALMODEL_H_
