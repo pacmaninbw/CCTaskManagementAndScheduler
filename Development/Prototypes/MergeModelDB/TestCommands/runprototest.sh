d93a689d15f2a08fe411daac0f89c242f124515a
pacmaninbw <paul.chernick@chernicksw.com>
2025-11-09 15:42:54 -0800

    Obly redirect stdout to a file, show failures.

--- Development/Prototypes/MergeModelDB/TestCommands/runprototest.sh
+++ Development/Prototypes/MergeModelDB/TestCommands/runprototest.sh
@@ -6,6 +6,6 @@ rm -f build/CMakeFiles/protoPersonalPlanner.dir/common/*.gcda
 rm -f build/CMakeFiles/protoPersonalPlanner.dir/Models/*.gcda
 rm -f build/CMakeFiles/protoPersonalPlanner.dir/Testing/*.gcda
 mysql -u $sqluser -p$sqlpassword < PlannerTaskScheduleDB.sql
-protoPersonalPlanner -u $sqluser -p $sqlpassword >& Testing/testOut.txt
+protoPersonalPlanner -u $sqluser -p $sqlpassword > Testing/testOut.txt
 echo "Diff"
 diff Testing/testOut.txt Testing/testOut_forDiff.txt
