673bd165d6e6febd4aa58ad6da0d1277e27c9d1e
pacmaninbw <paul.chernick@chernicksw.com>
2025-12-21 08:27:16 -0800

    Added functions to convert constant strings to date and date time. This will be used primarily for testing.

--- Development/Prototypes/MergeModelDB/common/commonUtilities.cpp
+++ Development/Prototypes/MergeModelDB/common/commonUtilities.cpp
@@ -1,5 +1,9 @@
 #include <chrono>
 #include "commonUtilities.h"
+#include <format>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 unsigned int OneWeek = 7;
 unsigned int TwoWeeks = 14;
@@ -22,4 +26,34 @@ std::chrono::year_month_day getTodaysDateMinus(unsigned int offset)
     std::chrono::time_point<std::chrono::system_clock> pastDate = std::chrono::system_clock::now();
     pastDate -= std::chrono::days(offset);
     return std::chrono::floor<std::chrono::days>(pastDate);
+}
+
+std::chrono::year_month_day constantStringToChronoDate(std::string dateStr)
+{
+    std::chrono::year_month_day dateValue;
+    std::istringstream ss(dateStr);
+
+    ss >> std::chrono::parse("%Y-%m-%d", dateValue);
+    if (ss.fail())
+    {
+        std::cerr << std::format("{} Failed to parse string {} to year_month_day.", __func__, dateStr) << std::endl;
+    }
+
+    return dateValue;
+}
+
+std::chrono::system_clock::time_point constantStringToChronoTimePoint(std::string dateTimeStr)
+{
+    std::chrono::system_clock::time_point dateTimeValue;
+
+ 
+    std::stringstream ss(dateTimeStr);
+    // Use std::chrono::from_stream with the desired format specifier
+    ss >> std::chrono::parse("%Y-%m-%d %H:%M:%S", dateTimeValue);
+
+    if (ss.fail()) {
+        std::cerr << std::format("{} Failed to parse string {} to time_point.", __func__, dateTimeStr) << std::endl;
+    }
+
+    return dateTimeValue;
 }
--- Development/Prototypes/MergeModelDB/common/commonUtilities.h
+++ Development/Prototypes/MergeModelDB/common/commonUtilities.h
@@ -1,6 +1,7 @@
 #ifndef COMMONUTILITIES_H_
 #define COMMONUTILITIES_H_
 #include <chrono>
+#include <string>
 
 extern unsigned int OneWeek;
 extern unsigned int TwoWeeks;
@@ -8,5 +9,7 @@ extern unsigned int TwoWeeks;
 extern std::chrono::year_month_day getTodaysDate();
 extern std::chrono::year_month_day getTodaysDatePlus(unsigned int offset);
 extern std::chrono::year_month_day getTodaysDateMinus(unsigned int offset);
+extern std::chrono::year_month_day constantStringToChronoDate(std::string dateStr);
+extern std::chrono::system_clock::time_point constantStringToChronoTimePoint(std::string dateTimeStr);
 
 #endif // COMMONUTILITIES_H_
