ae9f54f724f6f070df25e5b31d3967a8b41938c4
pacmaninbw <paul.chernick@chernicksw.com>
2026-01-18 18:32:00 -0800

    Basic read and write added, constructor for existing notes added.

--- Development/Prototypes/PP2WithSubDirs/GuiModels/GuiNoteModel.cpp
+++ Development/Prototypes/PP2WithSubDirs/GuiModels/GuiNoteModel.cpp
@@ -1,5 +1,28 @@
+// Project Header Files
+#include "NoteModel.h"
+
+// QT Header Files
 #include "GuiNoteModel.h"
 
 GuiNoteModel::GuiNoteModel(QObject *parent)
-    : QObject{parent}
+    : QObject{parent},
+    m_UserId{0},
+    m_DBNoteDataptr{nullptr}
 {}
+
+GuiNoteModel::GuiNoteModel(std::shared_ptr<NoteModel> dbNoteDataptr, QObject *parent)
+    : GuiNoteModel(parent)
+{
+    m_DBNoteDataptr = dbNoteDataptr;
+    m_UserId = dbNoteDataptr->getUserId();
+    m_Content = QString::fromStdString(dbNoteDataptr->getContent());
+}
+
+void GuiNoteModel::setContent(QString newContent)
+{
+    if (m_Content != newContent)
+    {
+        m_Content = newContent;
+        emit contentChanged();
+    }
+}
