28eb9f4004360854a8860146cd1de7a51e0a80bc
pacmaninbw <paul.chernick@chernicksw.com>
2026-02-06 06:41:05 -0800

    Fixed bug in qDateTimeToChrono.

--- Development/Prototypes/PP3Rework/GuiModels/stdChronoToQTConversions.cpp
+++ Development/Prototypes/PP3Rework/GuiModels/stdChronoToQTConversions.cpp
@@ -14,13 +14,14 @@ QDateTime chronoTimePointToQDateTime(std::chrono::system_clock::time_point input
 std::chrono::system_clock::time_point qDateTimeToChrono(const QDateTime& qtDateTime) {
     // Ensure the QDateTime is in UTC for a direct conversion to std::chrono::system_clock
     // which typically uses the Unix epoch (UTC) as its reference.
+
     QDateTime utcDateTime = qtDateTime;
     if (qtDateTime.timeZone() != QTimeZone::utc()) {
         utcDateTime = qtDateTime.toUTC();
     }
 
     // Get milliseconds since the epoch
-    qint64 msecsSinceEpoch = utcDateTime.currentMSecsSinceEpoch();
+    qint64 msecsSinceEpoch = utcDateTime.toMSecsSinceEpoch();
 
     // Create a std::chrono::milliseconds duration
     std::chrono::milliseconds durationSinceEpoch(msecsSinceEpoch);
@@ -28,7 +29,7 @@ std::chrono::system_clock::time_point qDateTimeToChrono(const QDateTime& qtDateT
     // Create the std::chrono::system_clock::time_point
     std::chrono::system_clock::time_point timePoint = std::chrono::system_clock::from_time_t(0) + durationSinceEpoch;
 
-    return timePoint;
+\    return timePoint;
 }
 
 std::chrono::year_month_day qDateToChrono(const QDate &qdate)
