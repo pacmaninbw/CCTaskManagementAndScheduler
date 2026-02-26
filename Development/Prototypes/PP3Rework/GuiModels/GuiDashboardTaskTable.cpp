7238da8850b29a96d4255b9678b12565c6727f6f
pacmaninbw <paul.chernick@chernicksw.com>
2026-02-26 07:42:17 -0800

    Inherit from GuiTaskTableBase rather than from QAbstractTableModel.

--- Development/Prototypes/PP3Rework/GuiModels/GuiDashboardTaskTable.cpp
+++ Development/Prototypes/PP3Rework/GuiModels/GuiDashboardTaskTable.cpp
@@ -1,90 +1,24 @@
 // Project Header Files
-#include "TaskList.h"
 #include "GuiDashboardTaskTable.h"
 #include "GuiTaskModel.h"
 #include "GuiUserModel.h"
 
 // QT Header Files
-#include <QAbstractTableModel>
-#include <QList>
-#include <QObject>
-#include <QString>
 
 // Standard C++ Header Files
-#include <chrono>
-#include <ranges>
 
 
 GuiDashboardTaskTable::GuiDashboardTaskTable(QObject *parent)
-    : QAbstractTableModel(parent),
-    m_UserDataPtr{nullptr}
+    : GuiTaskTableBase(parent)
 {}
 
 GuiDashboardTaskTable::GuiDashboardTaskTable(GuiUserModel *userDataPtr, QObject *parent)
-    : QAbstractTableModel(parent),
-    m_UserDataPtr{userDataPtr}
+    : GuiTaskTableBase(userDataPtr, parent)
 {
-}
-
-void GuiDashboardTaskTable::setUserRefillTable(GuiUserModel *userDataPtr)
-{
-    m_UserDataPtr = userDataPtr;
-    fillTable();
-}
-
-void GuiDashboardTaskTable::fillTable()
-{
-    if (!m_UserDataPtr || m_UserDataPtr->getDbUserId() == 0)
-    {
-        makeFakeQList();
-        return;
-    }
-
-    std::chrono::year_month_day searchDate = getTodaysDatePlus(TwoWeeks);
-    TaskList currentUserTaskList;
-    TaskListValues userTasks = currentUserTaskList.getDefaultDashboardTaskList(m_UserDataPtr->getDbUserId(), searchDate);
-
-    if (userTasks.empty())
-    {
-        makeFakeQList();
-        return;
-    }
-
-    if (!m_data.empty())
-    {
-        qDeleteAll(m_data.begin(), m_data.end());
-        m_data.clear();
-    }
-    for (const auto& dbTaskPtr: userTasks)
-    {
-        GuiTaskModel* modelData = new GuiTaskModel(dbTaskPtr, this);
-        m_data.append(modelData);
-    }
-}
-
-void GuiDashboardTaskTable::append(GuiTaskModel* taskData)
-{
-    beginInsertRows(QModelIndex(), m_data.count(), m_data.count());
-
-    m_data.append(taskData);
-    
-    endInsertRows();
-
-}
-
-void GuiDashboardTaskTable::clearData()
-{
-    beginResetModel();
-
-    qDeleteAll(m_data.begin(), m_data.end());
-    m_data.clear();
-
-    endResetModel();
 }
 
 QVariant GuiDashboardTaskTable::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    // FIXME: Implement me!
     if (orientation != Qt::Horizontal || role != Qt::DisplayRole) return {};
     switch (section) {
         case 0: return "PG";
@@ -103,16 +37,6 @@ bool GuiDashboardTaskTable::setHeaderData(
         return true;
     }
     return false;
-}
-
-int GuiDashboardTaskTable::rowCount(const QModelIndex &parent) const
-{
-    if (parent.isValid())
-    {
-        return 0;
-    }
-
-    return m_data.count();
 }
 
 int GuiDashboardTaskTable::columnCount(const QModelIndex &parent) const
@@ -161,55 +85,7 @@ Qt::ItemFlags GuiDashboardTaskTable::flags(const QModelIndex &index) const
     return QAbstractItemModel::flags(index) | Qt::ItemIsEditable; // FIXME: Implement me!
 }
 
-bool GuiDashboardTaskTable::insertRows(int position, int count, const QModelIndex &parent)
-{
-    Q_UNUSED(parent);
-    beginInsertRows(QModelIndex(), position, position + count - 1);
-
-    for (int row = 0; row < count; ++row)
-    {
-        m_data.insert(position, nullptr);
-    }
-
-    endInsertRows();
-
-    return true;
-}
-
-
-bool GuiDashboardTaskTable::removeRows(int position, int count, const QModelIndex &parent)
-{
-    Q_UNUSED(parent);
-    beginRemoveRows(QModelIndex(), position, position + count - 1);
-
-    for (int row = 0; row < count; ++row)
-    {
-        GuiTaskModel* data = m_data.at(position);
-        delete data;
-        m_data.removeAt(position);
-    }
-
-    endRemoveRows();
-
-    return true;
-}
-
-QModelIndex GuiDashboardTaskTable::index(int row, int column, const QModelIndex &parent) const
-{
-    if (!hasIndex(row, column, parent))
-    {
-        return QModelIndex();
-    }
-
-    GuiTaskModel* taskModelItem = m_data.at(row);
-    if (taskModelItem)
-    {
-        return createIndex(row, column, taskModelItem);
-    }
-
-    return QModelIndex();
-}
-
+#if 0
 void GuiDashboardTaskTable::makeFakeQList()
 {
     if (!m_data.isEmpty())
@@ -229,3 +105,4 @@ void GuiDashboardTaskTable::makeFakeQList()
         }
     }
 }
+#endif
--- Development/Prototypes/PP3Rework/GuiModels/GuiDashboardTaskTable.h
+++ Development/Prototypes/PP3Rework/GuiModels/GuiDashboardTaskTable.h
@@ -1,21 +1,14 @@
 #ifndef GUIDASHBOARDTASKTABLE_H
 #define GUIDASHBOARDTASKTABLE_H
 
-class GuiTaskModel;
-class GuiUserModel;
-
 // Project Header Files
+#include "GuiTaskTableBase.h"
 
 // QT Header Files
-#include <QAbstractTableModel>
-#include <QList>
-#include <QObject>
-#include <QString>
 
 // Standard C++ Header Files
 
-
-class GuiDashboardTaskTable : public QAbstractTableModel
+class GuiDashboardTaskTable : public GuiTaskTableBase
 {
     Q_OBJECT
 
@@ -23,27 +16,16 @@ public:
 
     explicit GuiDashboardTaskTable(QObject *parent = nullptr);
     explicit GuiDashboardTaskTable(GuiUserModel *userDataPtr, QObject* parent=nullptr);
-    void setUserRefillTable(GuiUserModel *userDataPtr);
-    void fillTable();
-    void append(GuiTaskModel* taskData);
-    void clearData();
 
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
     bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role = Qt::EditRole) override;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
-    bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
-    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
-
 
 private:
-    void makeFakeQList();
-    GuiUserModel *m_UserDataPtr;
-    QList<GuiTaskModel*> m_data;
+//    void makeFakeQList();
 };
 
 #endif // GUIDASHBOARDTASKTABLE_H
