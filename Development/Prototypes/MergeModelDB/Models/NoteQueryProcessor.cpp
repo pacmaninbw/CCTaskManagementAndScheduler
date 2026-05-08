f461c47f1e8bd2d8c80256b5e3c92d936b33ea00
pacmaninbw <paul.chernick@chernicksw.com>
2026-05-08 07:32:54 -0700

    Re-implemented note queries using a dynamic interface. The queries no longer contain the physical database information. All queries are implemented using stored procedures in the database.

--- Development/Prototypes/MergeModelDB/Models/NoteQueryProcessor.cpp
+++ Development/Prototypes/MergeModelDB/Models/NoteQueryProcessor.cpp
@@ -4,18 +4,28 @@
 #include "NoteModel.h"
 
 // Standard C++ Header Files
+#include <algorithm>
 #include <chrono>
+#include <exception>
 #include <format>
 #include <iostream>
+#include <iterator>
+#include <optional>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 NoteQueryProcessor::NoteQueryProcessor()
-: QueryProcessor<NoteModel>()
+: CoreDBInterface()
 {
-
+    requiredColumns =  {"idUserNotes", "UserID", "Content", "Hidden", "NotationDateTime",  "LastUpdate"};
+    for (auto columnName: requiredColumns)
+    {
+        columnToIndexMap.push_back(columnName);
+    }
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUser(std::size_t userId) noexcept
+NoteList NoteQueryProcessor::getAllNotesForUser(std::size_t userId) noexcept
 {
     errorMessages.clear();
 /*
@@ -25,8 +35,8 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUser(std::size_t user
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatSelectByUserId(userId);
-        return runQueryFillNoteQueryProcessor();
+        boost::mysql::results localResult = runQueryAsync(formatSelectByUserId(userId));
+        return processResults(localResult);
     }
 
     catch(const std::exception& e)
@@ -34,18 +44,18 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUser(std::size_t user
         appendErrorMessage(e.what());
     }
     
-    return NoteQueryProcessorValues();
+    return NoteList();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::getNotesForUserSimlarToContent(std::size_t userId, std::string likeContent) noexcept
+NoteList NoteQueryProcessor::getNotesForUserSimlarToContent(std::size_t userId, std::string likeContent) noexcept
 {
     errorMessages.clear();
     appendErrorMessage("In NoteQueryProcessor::getNotesForUserSimlarToContent : ");
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatSelectByUserIdAndSimilarContent(userId, likeContent);
-        return runQueryFillNoteQueryProcessor();
+        boost::mysql::results localResult = runQueryAsync(formatSelectByUserIdAndSimilarContent(userId, likeContent));
+        return processResults(localResult);
     }
 
     catch(const std::exception& e)
@@ -53,10 +63,10 @@ NoteQueryProcessorValues NoteQueryProcessor::getNotesForUserSimlarToContent(std:
         appendErrorMessage(e.what());
     }
     
-    return NoteQueryProcessorValues();
+    return NoteList();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserCreatedInDatgeRange(
+NoteList NoteQueryProcessor::getAllNotesForUserCreatedInDatgeRange(
     std::size_t userId, std::chrono::year_month_day startDate, std::chrono::year_month_day endDate) noexcept
 {
     errorMessages.clear();
@@ -64,8 +74,8 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserCreatedInDatgeRan
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatSelectByUserIdAndCreationDateRange(userId, startDate, endDate);
-        return runQueryFillNoteQueryProcessor();
+        boost::mysql::results localResult = runQueryAsync(formatSelectByUserIdAndCreationDateRange(userId, startDate, endDate));
+        return processResults(localResult);
     }
 
     catch(const std::exception& e)
@@ -73,10 +83,10 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserCreatedInDatgeRan
         appendErrorMessage(e.what());
     }
     
-    return NoteQueryProcessorValues();
+    return NoteList();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserEditedInDatgeRange(
+NoteList NoteQueryProcessor::getAllNotesForUserEditedInDatgeRange(
     std::size_t userId, std::chrono::year_month_day startDate, std::chrono::year_month_day endDate) noexcept
 {
     errorMessages.clear();
@@ -84,8 +94,8 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserEditedInDatgeRang
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatSelectByUserIdAndUpdateDateRange(userId, startDate, endDate);
-        return runQueryFillNoteQueryProcessor();
+        boost::mysql::results localResult = runQueryAsync(formatSelectByUserIdAndUpdateDateRange(userId, startDate, endDate));
+        return processResults(localResult);
     }
 
     catch(const std::exception& e)
@@ -93,18 +103,18 @@ NoteQueryProcessorValues NoteQueryProcessor::getAllNotesForUserEditedInDatgeRang
         appendErrorMessage(e.what());
     }
     
-    return NoteQueryProcessorValues();
+    return NoteList();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::getDashboardNoteTable(std::size_t userId, std::chrono::year_month_day searchDate) noexcept
+NoteList NoteQueryProcessor::getDashboardNoteTable(std::size_t userId, std::chrono::year_month_day searchDate) noexcept
 {
     errorMessages.clear();
     appendErrorMessage("In NoteQueryProcessor::getDashboardNoteTable : ");
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatGetNotesFromUserForDate(userId, searchDate);
-        return runQueryFillNoteQueryProcessor();
+        boost::mysql::results localResult = runQueryAsync(formatGetNotesFromUserForDate(userId, searchDate));
+        return processResults(localResult);
     }
 
     catch(const std::exception& e)
@@ -112,40 +122,197 @@ NoteQueryProcessorValues NoteQueryProcessor::getDashboardNoteTable(std::size_t u
         appendErrorMessage(e.what());
     }
     
-    return NoteQueryProcessorValues();
+    return NoteList();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::runQueryFillNoteQueryProcessor()
+std::string NoteQueryProcessor::formatSelectByUserId(std::size_t userId)
 {
-    if (firstFormattedQuery.empty())
-    {
-        appendErrorMessage(std::format("Formatting select multiple notes query string failed {}",
-            queryGenerator->getAllErrorMessages()));
-        return NoteQueryProcessorValues();
-    }
-    
-    if (runFirstQuery())
-    {
-        return fillNoteQueryProcessor();
-    }
+    initFormatOptions();
+    boost::mysql::format_context fctx(format_opts.value());
+    boost::mysql::format_sql_to(fctx, "CALL GetAllUndeletedNotesForUser({})", userId);
 
-    return NoteQueryProcessorValues();
+    return std::move(fctx).get().value();
 }
 
-NoteQueryProcessorValues NoteQueryProcessor::fillNoteQueryProcessor()
+std::string NoteQueryProcessor::formatSelectByUserIdAndSimilarContent(std::size_t userId, std::string similarContent)
 {
-    NoteQueryProcessorValues NoteQueryProcessor;
+    initFormatOptions();
+    boost::mysql::format_context fctx(format_opts.value());
+    boost::mysql::format_sql_to(fctx, "CALL GetNotesForUserSimlarToContent({}, {})", userId, similarContent);
 
-    for (auto NoteID: primaryKeyResults)
-    {
-        NoteModel_shp newNote = std::make_shared<NoteModel>(NoteModel());
-        newNote->selectByNoteID(NoteID);
-        NoteQueryProcessor.push_back(newNote);
-    }
-
-    return NoteQueryProcessor;
+    return std::move(fctx).get().value();
 }
 
+std::string NoteQueryProcessor::formatSelectByUserIdAndCreationDateRange(
+    std::size_t userId, std::chrono::year_month_day startDay, std::chrono::year_month_day endDay)
+{
+    initFormatOptions();
+    boost::mysql::format_context fctx(format_opts.value());
+    boost::mysql::format_sql_to(fctx, "CALL GetAllNotesForUserCreatedInDatgeRange({}, {}, {})", userId,
+        stdchronoDateToBoostMySQLDate(startDay), stdchronoDateToBoostMySQLDate(endDay));
+
+    return std::move(fctx).get().value();
+}
+
+std::string NoteQueryProcessor::formatSelectByUserIdAndUpdateDateRange(
+    std::size_t userId, std::chrono::year_month_day startDay, std::chrono::year_month_day endDay)
+{
+    initFormatOptions();
+    boost::mysql::format_context fctx(format_opts.value());
+    boost::mysql::format_sql_to(fctx, "CALL GetAllNotesForUserEditedInDatgeRange({}, {}, {})", userId,
+        stdchronoDateToBoostMySQLDate(startDay), stdchronoDateToBoostMySQLDate(endDay));
+
+    return std::move(fctx).get().value();
+}
+
+std::string NoteQueryProcessor::formatGetNotesFromUserForDate(std::size_t userId, std::chrono::year_month_day searchDate)
+{
+    std::chrono::system_clock::time_point startDay(getLocalMidnight(searchDate));
+    std::chrono::hours mostHoursInDay{23};
+    std::chrono::minutes minutesInLastHour{59};
+    std::chrono::system_clock::duration oneDayOffset = mostHoursInDay + minutesInLastHour;
+    std::chrono::system_clock::time_point endDay(startDay + oneDayOffset);
+
+    initFormatOptions();
+    boost::mysql::format_context fctx(format_opts.value());
+    boost::mysql::format_sql_to(fctx, "CALL GetDashboardNoteTable({}, {}, {})", userId,
+        stdChronoTimePointToBoostDateTime(startDay), stdChronoTimePointToBoostDateTime(endDay));
+
+    return std::move(fctx).get().value();
+}
+
+/*
+ * Map the column names to the indexes for the columns, the order of the columns
+ * in the result set is not guarenteed. Ensure that the columns necessary to fill
+ * the model are present. Currently there is no error if there are too many fields.
+ */
+void NoteQueryProcessor::mapColumnNameToIndex(boost::mysql::resultset_view &noteQueryresultSet)
+{
+    std::vector<std::string> columnNames;
+    bool hasAllRequiredColumns = true;
+
+    for (auto metaIter: noteQueryresultSet.meta())
+    {
+        columnNames.push_back(metaIter.column_name());
+    }
+
+    for (std::size_t i = 0; i < columnToIndexMap.size(); ++i)
+    {
+        std::string nameToFind = columnToIndexMap[i].columnName;
+        auto iterToIndex = std::find(columnNames.begin(), columnNames.end(), nameToFind);
+        if (iterToIndex != columnNames.end())
+        {
+            columnToIndexMap[i].columnIndex = std::distance(columnNames.begin(), iterToIndex);
+        }
+        else
+        {
+            appendErrorMessage(std::format("Required field {} not found in results", nameToFind));
+            hasAllRequiredColumns = false;
+        }
+    }
+
+    if (!hasAllRequiredColumns)
+    {
+        throw std::out_of_range("Results missing required fields");
+    }
+
+    fillRequiredIndexes();
+}
+
+void NoteQueryProcessor::assignValueToIndex(std::string columnName, std::size_t &columnIndex)
+{
+        auto iterToIndex = std::find_if(columnToIndexMap.begin(), columnToIndexMap.end(),
+            [columnName](const ColumnNameToIndexmapping& ctim){ return ctim.columnName == columnName; });
+        if (iterToIndex != columnToIndexMap.end())
+        {
+            if (iterToIndex->columnIndex.has_value())
+            {
+                columnIndex = iterToIndex->columnIndex.value();
+            }
+            else
+            {
+                appendErrorMessage(std::format("NULL Value to index for {}", columnName));
+            }
+        }
+        else
+        {
+            appendErrorMessage(std::format("Column Name: {} NOT FOUND in assignValueToIndex()", columnName));
+        }
+}
+
+void NoteQueryProcessor::fillRequiredIndexes()
+{
+    assignValueToIndex("idUserNotes", noteIDX);
+    assignValueToIndex("UserID", userIDX);
+    assignValueToIndex("Content", contentIDX);
+    assignValueToIndex("Hidden", hiddenIDX);
+    assignValueToIndex("NotationDateTime", createdIDX);
+    assignValueToIndex("LastUpdate", lastmodIDX);
+}
+
+/*
+ * The boost::mysql::results class should contain all the results from the current
+ * query, the results may span multiple result sets. The processResults method
+ * should return all the results found in a single list.
+ */
+NoteList NoteQueryProcessor::processResults(boost::mysql::results &noteQueryResults)
+{
+    NoteList queryResultValues;
+
+    for (std::size_t i = 0; i < noteQueryResults.size(); ++i)
+    {
+        boost::mysql::resultset_view resultview = noteQueryResults[i];
+        NoteList intermediateResults = processResultSet(resultview);
+        queryResultValues.insert(queryResultValues.end(), intermediateResults.begin(), intermediateResults.end());
+    }
+
+    return queryResultValues;
+}
+
+/*
+ * Each boost::mysql result set contain a list of the column names in the result set.
+ * Given the current implementation these columns should be in the same order for 
+ * every result set, but we can't depend on that so we need to map the column names
+ * to the indexes for every result set. For performance reasons we want to map the
+ * indexes once per result set rather than doing a string search for each row of 
+ * data. Create an object of the proper model for each row of data in the result set.
+ */
+NoteList NoteQueryProcessor::processResultSet(boost::mysql::resultset_view &noteQueryresultSet)
+{
+    NoteList intermediateResults;
+
+    if (noteQueryresultSet.rows().num_columns() > 0)
+    {
+        mapColumnNameToIndex(noteQueryresultSet);
+        for (auto rv: noteQueryresultSet.rows())
+        {
+            intermediateResults.push_back(processResultRow(rv));
+        }
+    }
+
+    return intermediateResults;
+}
+
+NoteModel_shp NoteQueryProcessor::processResultRow(boost::mysql::row_view &noteQueryRow)
+{
+    std::size_t noteId = noteQueryRow.at(noteIDX).as_uint64();
+    std::size_t userID = noteQueryRow.at(userIDX).as_uint64();
+    std::string content = noteQueryRow.at(contentIDX).as_string();
+    bool deleted = false;
+    if (!noteQueryRow.at(hiddenIDX).is_null())
+    {
+        deleted = noteQueryRow.at(hiddenIDX).as_int64() == 1? true : false;
+    }
+
+    std::chrono::system_clock::time_point creationDate = boostMysqlDateTimeToChronoTimePoint(noteQueryRow.at(createdIDX).as_datetime());
+    std::chrono::system_clock::time_point lastUpdate = boostMysqlDateTimeToChronoTimePoint(noteQueryRow.at(lastmodIDX).as_datetime());
+
+    NoteModel_shp noteListMember = std::make_shared<NoteModel>(noteId, userID, content, creationDate, lastUpdate, deleted);
+
+    return noteListMember;
+}
+
+#if 0
 std::vector<ListExceptionTestElement> NoteQueryProcessor::initListExceptionTests() noexcept 
 {
     std::vector<ListExceptionTestElement> exceptionTests;
@@ -215,3 +382,5 @@ TestStatus NoteQueryProcessor::testExceptionsGetDashboardNoteTable() noexcept
         std::bind(&NoteQueryProcessor::getDashboardNoteTable, this, std::placeholders::_1, std::placeholders::_2), 
         testUserId, searchDate);
 }
+#endif
+
--- Development/Prototypes/MergeModelDB/Models/NoteQueryProcessor.h
+++ Development/Prototypes/MergeModelDB/Models/NoteQueryProcessor.h
@@ -2,39 +2,80 @@
 #define NOTEQUERYPROCESSOR_H_
 
 // Project Header Files
-#include "QueryProcessor.h"
+//#include "QueryProcessor.h"
+#include "CoreDBInterface.h"
 #include "NoteModel.h"
+
+// External Libraries
+#include <boost/asio.hpp>
+#include <boost/mysql.hpp>
+
 
 // Standard C++ Header Files
 #include <chrono>
 #include <iostream>
+#include <optional>
 #include <string>
+#include <vector>
 
-using NoteQueryProcessorValues = std::vector<NoteModel_shp>;
+using NoteList = std::vector<NoteModel_shp>;
 
-class NoteQueryProcessor : public QueryProcessor<NoteModel>
+struct ColumnNameToIndexmapping
+{
+    std::string columnName;
+    std::optional<std::size_t> columnIndex;
+    ColumnNameToIndexmapping(std::string name) : columnName{name}{};
+};
+
+class NoteQueryProcessor : public CoreDBInterface
 {
 public:
     NoteQueryProcessor();
     virtual ~NoteQueryProcessor() = default;
-    NoteQueryProcessorValues getAllNotesForUser(std::size_t userId) noexcept;
-    NoteQueryProcessorValues getNotesForUserSimlarToContent(std::size_t userId, std::string likeContent) noexcept;
-    NoteQueryProcessorValues getAllNotesForUserCreatedInDatgeRange(std::size_t userId,
+    NoteList getAllNotesForUser(std::size_t userId) noexcept;
+    NoteList getNotesForUserSimlarToContent(std::size_t userId, std::string likeContent) noexcept;
+    NoteList getAllNotesForUserCreatedInDatgeRange(std::size_t userId,
         std::chrono::year_month_day startDate, std::chrono::year_month_day endDate) noexcept;
-    NoteQueryProcessorValues getAllNotesForUserEditedInDatgeRange(std::size_t userId,
+    NoteList getAllNotesForUserEditedInDatgeRange(std::size_t userId,
         std::chrono::year_month_day startDate, std::chrono::year_month_day endDate) noexcept;
-    NoteQueryProcessorValues getDashboardNoteTable(std::size_t userId, std::chrono::year_month_day searchDate) noexcept;
+    NoteList getDashboardNoteTable(std::size_t userId, std::chrono::year_month_day searchDate) noexcept;
 
 private:
-    NoteQueryProcessorValues runQueryFillNoteQueryProcessor();
-    NoteQueryProcessorValues fillNoteQueryProcessor();
+    std::string formatSelectByUserId(std::size_t userId);
+    std::string formatSelectByUserIdAndSimilarContent(std::size_t userId, std::string similarContent);
+    std::string formatSelectByUserIdAndCreationDateRange(std::size_t userId, std::chrono::year_month_day startDay,
+        std::chrono::year_month_day endDay);
+    std::string formatSelectByUserIdAndUpdateDateRange(std::size_t userId, std::chrono::year_month_day startDay,
+        std::chrono::year_month_day endDay);
+    std::string formatGetNotesFromUserForDate(std::size_t userId, std::chrono::year_month_day searchDate);
+    void mapColumnNameToIndex(boost::mysql::resultset_view& noteQueryresultSet);
+    void assignValueToIndex(std::string columnName, std::size_t& columnIndex);
+    void fillRequiredIndexes();
+    NoteList processResults(boost::mysql::results& noteQueryResults);
+    NoteList processResultSet(boost::mysql::resultset_view& noteQueryresultSet);
+    NoteModel_shp processResultRow(boost::mysql::row_view& noteQueryRow);
 
+    std::vector<ColumnNameToIndexmapping> columnToIndexMap;
+    std::vector<std::string> requiredColumns;
+
+    boost::mysql::constant_string_view baseQuery = "SELECT * FROM UserNotes ";
+
+    std::size_t noteIDX = 0xffff;
+    std::size_t userIDX = 0xffff;
+    std::size_t contentIDX = 0xffff;
+    std::size_t hiddenIDX = 0xffff;
+    std::size_t createdIDX = 0xffff;
+    std::size_t lastmodIDX = 0xffff;
+
+
+#if  0
     virtual std::vector<ListExceptionTestElement> initListExceptionTests() noexcept override;
     TestStatus testExceptionsGetAllNotes() noexcept;
     TestStatus testExceptionsGetNotesForUserLikeContent() noexcept;
     TestStatus testExceptionsGetNotesForUserCreatedDateRange() noexcept;
     TestStatus testExceptionsGetNotesForUserEditedDateRange() noexcept;
     TestStatus testExceptionsGetDashboardNoteTable() noexcept;
+#endif
 };
 
 #endif // NOTEQUERYPROCESSOR_H_
