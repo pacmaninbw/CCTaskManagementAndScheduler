f25ecc180d60c66c2aea50eef94f8b195481d02e
pacmaninbw <paul.chernick@chernicksw.com>
2026-05-12 13:44:55 -0700

    User lists are now generated using the new query process design.

--- Development/Prototypes/MergeModelDB/Models/UserQueryProcessor.cpp
+++ Development/Prototypes/MergeModelDB/Models/UserQueryProcessor.cpp
@@ -10,42 +10,78 @@
 UserQueryProcessor::UserQueryProcessor()
 : QueryProcessor<UserModel>()
 {
-
+    requiredColumns =  {"UserID", "Organization_ID", "LastName", "FirstName", "MiddleInitial",  "EmailAddress", "LoginName", "HashedPassWord", "UserAdded", "LastLogin", "Preferences", "Hidden"};
+    for (auto columnName: requiredColumns)
+    {
+        columnToIndexMap.push_back(columnName);
+    }
 }
 
-UserQueryProcessorValues UserQueryProcessor::getAllUsers() noexcept
+UserModelList UserQueryProcessor::getAllUsers() noexcept
 {
     errorMessages.clear();
+
+    appendErrorMessage("In NoteQueryProcessor::getAllNotesForUser : ");
     
-    UserQueryProcessorValues allUsers;
+    UserModelList allUsers;
 
     try
     {
-        firstFormattedQuery = queryGenerator->formatGetAllUsersQuery();
-        if (firstFormattedQuery.empty())
-        {
-            appendErrorMessage(std::format("Formatting getAllUser query string failed {}",
-                queryGenerator->getAllErrorMessages()));
-            return allUsers;
-        }
-        
-        if (runFirstQuery())
-        {
-            for (auto userID: primaryKeyResults)
-            {
-                UserModel_shp newUser = std::make_shared<UserModel>(UserModel());
-                newUser->selectByUserID(userID);
-                allUsers.push_back(newUser);
-            }
-        }
+        initFormatOptions();
+        boost::mysql::format_context fctx(format_opts.value());
+        boost::mysql::results localResult = runQueryAsync(boost::mysql::format_sql(
+            format_opts.value(), "SELECT * FROM UserProfile "));
+        allUsers = processResults(localResult);
+        std::cout << "UserQueryProcessor::getAllUsers() allusers " << allUsers.size() << std::endl;
     }
-
+        
     catch(const std::exception& e)
     {
         appendErrorMessage(std::format("In UserQueryProcessor::getAllUsers : {}", e.what()));
     }
     
     return allUsers;
+}
+
+UserModel_shp UserQueryProcessor::processResultRow(boost::mysql::row_view &queryRow)
+{
+    std::size_t userId = queryRow.at(UserIdIdx).as_uint64();
+    std::string lastName = queryRow.at(LastNameIdx).as_string();
+    std::string firstName = queryRow.at(FirstNameIdx).as_string();
+    std::string middleInitial = queryRow.at(MiddleInitialIdx).as_string();
+    std::string email = queryRow.at(EmailAddressIdx).as_string();
+    std::string loginName = queryRow.at(LoginNameIdx).as_string();
+    std::string password = queryRow.at(PasswordIdx).as_string();
+    std::chrono::system_clock::time_point created = (boostMysqlDateTimeToChronoTimePoint(queryRow.at(UserAddedIdx).as_datetime()));
+    std::chrono::system_clock::time_point lastLogin;
+    std::size_t organizationId = 0;
+    if (!queryRow.at(LastLoginIdx).is_null())
+    {
+        lastLogin = boostMysqlDateTimeToChronoTimePoint(queryRow.at(LastLoginIdx).as_datetime());
+    }
+    std::string preferences = queryRow.at(PreferencesIdx).as_string();
+    if (!queryRow.at(OrganizationIdx).is_null())
+    {
+        organizationId = queryRow.at(OrganizationIdx).as_uint64();
+    }
+
+    return std::make_shared<UserModel>(userId, lastName, firstName, middleInitial, email, loginName, password, preferences, created, lastLogin, organizationId);
+}
+
+void UserQueryProcessor::fillRequiredIndexes()
+{
+    assignValueToIndex("UserID", UserIdIdx);
+    assignValueToIndex("Organization_ID", OrganizationIdx);
+    assignValueToIndex("LastName", LastNameIdx);
+    assignValueToIndex("FirstName", FirstNameIdx);
+    assignValueToIndex("MiddleInitial", MiddleInitialIdx);
+    assignValueToIndex("EmailAddress", EmailAddressIdx);
+    assignValueToIndex("LoginName", LoginNameIdx);
+    assignValueToIndex("HashedPassWord", PasswordIdx);
+    assignValueToIndex("UserAdded", UserAddedIdx);
+    assignValueToIndex("LastLogin", LastLoginIdx);
+    assignValueToIndex("Preferences", PreferencesIdx);
+    assignValueToIndex("Hidden", HiddenIdx);
 }
 
 std::vector<ListExceptionTestElement> UserQueryProcessor::initListExceptionTests() noexcept 
--- Development/Prototypes/MergeModelDB/Models/UserQueryProcessor.h
+++ Development/Prototypes/MergeModelDB/Models/UserQueryProcessor.h
@@ -9,18 +9,34 @@
 #include <format>
 #include <iostream>
 
-using UserQueryProcessorValues = std::vector<UserModel_shp>;
+using UserModelList = std::vector<UserModel_shp>;
 
 class UserQueryProcessor : public QueryProcessor<UserModel>
 {
 public:
     UserQueryProcessor();
     virtual ~UserQueryProcessor() = default;
-    UserQueryProcessorValues getAllUsers() noexcept;
+    UserModelList getAllUsers() noexcept;
 
 private:
+    virtual UserModel_shp processResultRow(boost::mysql::row_view& queryRow) override;
+    virtual void fillRequiredIndexes() override;
+
     virtual std::vector<ListExceptionTestElement> initListExceptionTests() noexcept override;
     TestStatus testExceptionsGetAllUsers() noexcept;
+
+    std::size_t UserIdIdx = IndexNotSet;
+    std::size_t OrganizationIdx = IndexNotSet;
+    std::size_t LastNameIdx = IndexNotSet;
+    std::size_t FirstNameIdx = IndexNotSet;
+    std::size_t MiddleInitialIdx = IndexNotSet;
+    std::size_t EmailAddressIdx = IndexNotSet;
+    std::size_t LoginNameIdx = IndexNotSet;
+    std::size_t PasswordIdx = IndexNotSet;
+    std::size_t UserAddedIdx = IndexNotSet;
+    std::size_t LastLoginIdx = IndexNotSet;
+    std::size_t PreferencesIdx = IndexNotSet;
+    std::size_t HiddenIdx = IndexNotSet;
 };
 
 #endif // USERQUERYPROCESSOR_H_
