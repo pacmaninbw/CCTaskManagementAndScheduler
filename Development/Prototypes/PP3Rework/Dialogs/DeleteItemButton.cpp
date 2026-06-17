516a31e41055c96cfcf4656b2088d214e4c41149
pacmaninbw <paul.chernick@chernicksw.com>
2026-06-17 05:26:10 -0700

    Added a delete button subclass that is used by all object editors to delete / hide objects in the database.

---
+++ Development/Prototypes/PP3Rework/Dialogs/DeleteItemButton.cpp
@@ -0,0 +1,20 @@
+// Project Header Files
+#include "DeleteItemButton.h"
+
+// QT Header Files
+#include <QPalette>
+#include <QPushButton>
+#include <QString>
+
+// Standard C++ Header Files
+
+DeleteItemButton::DeleteItemButton(QString itemType)
+{
+    QString buttonText = "Delete " + itemType;
+    setText(buttonText);
+    setAutoFillBackground(true);
+    QPalette palette = this->palette();
+    palette.setColor(QPalette::ButtonText, Qt::black);
+    palette.setColor(QPalette::Button, Qt::red);
+    setPalette(palette);
+}
---
+++ Development/Prototypes/PP3Rework/Dialogs/DeleteItemButton.h
@@ -0,0 +1,13 @@
+#ifndef DELETEITEMBUTTON_H_
+#define DELETEITEMBUTTON_H_
+
+#include <QPushButton>
+#include <QString>
+
+class DeleteItemButton : public QPushButton
+{
+public:
+    DeleteItemButton(QString itemType);
+};
+
+#endif // DELETEITEMBUTTON_H_
