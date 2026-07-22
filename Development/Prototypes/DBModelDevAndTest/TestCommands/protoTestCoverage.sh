48224ae2d9ad8e3bcd535eb2684a14e2af9c3a98
pacmaninbw <paul.chernick@chernicksw.com>
2026-07-22 05:52:37 -0700

    Addressed the issues in the regression test scripts that were reported in the code review.  Add comments to the regression test shell commands Fixes #28 The regression test shell commands have code that repeats. Fixes #30 The Regression test shell scripts need error handling to prevent cascading errors Fixes #31

--- Development/Prototypes/DBModelDevAndTest/TestCommands/protoTestCoverage.sh
+++ Development/Prototypes/DBModelDevAndTest/TestCommands/protoTestCoverage.sh
@@ -1,21 +1,42 @@
+#
+# usage protoTestCoverage.sh SQLUSER SQLUSERPASSWORD
+#
+# Runs the regression test and generates test coverage information.
+# The regression test is run without the verbose flag. This provides
+# just the output from the tests. If any of the tests fail the vgprototest.sh
+# test script can be run to determine what is causing the failure.
+#
 #!/bin/sh
-sqluser="$1"
-sqlpassword="$2"
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/common/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/Models/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/Testing/*.gcda
-mkdir -p TestCoverage
-mysql -u $sqluser -p$sqlpassword < PlannerTaskScheduleDB.sql
-mysql -u $sqluser -p$sqlpassword < AdditionalFunctionalTestData.sql
+sqluser="${1:-no_username_supplied}"
+sqlpassword="${2:-no_password_supplied}"
+build_dir=build/CMakeFiles/protoPersonalPlanner.dir
+
+# Remove previously generated files
+rm -f $build_dir/*.gcda $build_dir/{common,Models,Testing}/*.gcda
+rm -rf TestCoverage
+
+#
+# Combine the data definitions with the test data and create the test database
+#
+cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql > combinedInput.sql
+mysql -u $sqluser -p$sqlpassword < combinedInput.sql || exit
+rm combinedInput.sql
+
+echo "Running Regression Tests"
 build/protoPersonalPlanner -u "$sqluser" -p "$sqlpassword" --time-tests > Testing/testOut.txt
+#
+# because of the diff, -e can't be used
+#
 echo "Diff"
 diff -w Testing/testOut.txt Testing/testOut_forDiff.txt
-cp build/protoPersonalPlanner TestCoverage
-cp build/CMakeFiles/protoPersonalPlanner.dir/*.gc* TestCoverage
-cp build/CMakeFiles/protoPersonalPlanner.dir/common/*.gc* TestCoverage
-cp build/CMakeFiles/protoPersonalPlanner.dir/Models/*.gc* TestCoverage
-cp build/CMakeFiles/protoPersonalPlanner.dir/Testing/*.gc* TestCoverage
+
+mkdir TestCoverage || exit
+echo "copy Gcov data"
+cp -t TestCoverage build/protoPersonalPlanner \
+    build/CMakeFiles/protoPersonalPlanner.dir/*.gc* \
+    build/CMakeFiles/protoPersonalPlanner.dir/*/*.gc*
+echo "Running Lcov"
 lcov --directory . --capture --output-file TestCoverage/protoPersonalPlanner_Total.info > lcovOut.txt 2>&1
+echo "Removing External Coverage" 
 lcov --remove TestCoverage/protoPersonalPlanner_Total.info '/usr/include/*' '*/boost/*' -o TestCoverage/protoPersonalPlanner_filtered.info
 genhtml TestCoverage/protoPersonalPlanner_filtered.info --output-directory TestCoverage
--- Development/Prototypes/DBModelDevAndTest/TestCommands/vgprototest.sh
+++ Development/Prototypes/DBModelDevAndTest/TestCommands/vgprototest.sh
@@ -1,11 +1,36 @@
+#
+# usage vgprototest.sh SQLUSER SQLUSERPASSWORD
+#
+#
+# runs a full regression test of the database models. All SQL commands are dumped to the output
+# The regression test is run through valgrind to check for memory leakage as well as memory
+# usage errors. The test database is recreated each time and provided with previous versions
+# of real data. The verbose output flag is used to generate additional debug information.
+#
 #!/bin/sh
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/common/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/Models/*.gcda
-rm -f build/CMakeFiles/protoPersonalPlanner.dir/Testing/*.gcda
-mysql -u "$1" -p"$2" < PlannerTaskScheduleDB.sql
-mysql -u "$1" -p"$2" < AdditionalFunctionalTestData.sql
-valgrind --track-origins=yes protoPersonalPlanner -u "$1" -p "$2" --verbose --time-tests 2>&1 | sed 's/^==[0-9]*== //' > Testing/valgrindOut.txt
+
+#
+# Since the diff is the last command the -e flag can be used to exit the script
+#
+set -e -o
+sqluser="${1:-no_username_supplied}"
+sqlpassword="${2:-no_password_supplied}"
+build_dir=build/CMakeFiles/protoPersonalPlanner.dir
+
+rm -f $build_dir/*.gcda
+rm -f $build_dir/{common,Models,Models}/*.gcda
+
+#
+# Combine the data definitions with the test data and create the test database
+#
+cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql > combinedInput.sql
+mysql -u $sqluser -p$sqlpassword < combinedInput.sql
+rm combinedInput.sql
+
+#
+# The linux command sed is run to remove the process id from the valgrind output to allow for comparing the output
+#
+valgrind --track-origins=yes protoPersonalPlanner -u "$sqluser" -p "$sqlpassword" --verbose --time-tests 2>&1 | sed 's/^==[0-9]*== //' > Testing/valgrindOut.txt
 echo "valgrind Diff"
 date
 diff -w Testing/valgrindOut.txt Testing/valgrindOut_forDiff.txt
