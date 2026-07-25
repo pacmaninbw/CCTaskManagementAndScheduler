fbd45c1a26333d65f3b9eff00a462f263618860d
pacmaninbw <paul.chernick@chernicksw.com>
2026-07-25 07:27:06 -0700

    Improved code based on recommendations from the code review.

--- Development/Prototypes/DBModelDevAndTest/TestCommands/protoTestCoverage.sh
+++ Development/Prototypes/DBModelDevAndTest/TestCommands/protoTestCoverage.sh
@@ -10,34 +10,30 @@
 set -e
 sqluser="${1:-no_username_supplied}"
 sqlpassword="${2:-no_password_supplied}"
-build_dir=build/CMakeFiles/protoPersonalPlanner.dir
 
 # Remove previously generated files
-rm -f $build_dir/*.gcda $build_dir/{common,Models,Testing}/*.gcda
 rm -rf TestCoverage
+find . -type f -name "*.gcda" -delete
 
-#
-# Combine the data definitions with the test data and create the test database
-#
 echo "Creating test database with test data"
-cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql > combinedInput.sql
-mysql -u $sqluser -p$sqlpassword < combinedInput.sql
-rm combinedInput.sql
+cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql | mysql -u $sqluser -p$sqlpassword
 
 echo "Running Regression Tests"
-build/protoPersonalPlanner -u "$sqluser" -p "$sqlpassword" --time-tests > Testing/testOut.txt
+# If regression tests fails the diff and test coverage reports are still valid
+build/protoPersonalPlanner -u "$sqluser" -p "$sqlpassword" --time-tests > Testing/testOut.txt || echo "REGRESSION TESTS FAILED!"
 
 echo "Diff"
-diff -w Testing/testOut.txt Testing/testOut_forDiff.txt  > protoTestDiff.txt || true
+diff -w Testing/testOut.txt Testing/testOut_forDiff.txt  > protoTestDiffOut.txt || true
 
 mkdir TestCoverage
 echo "copy Gcov data"
-cp -t TestCoverage build/protoPersonalPlanner \
-    build/CMakeFiles/protoPersonalPlanner.dir/*.gc* \
-    build/CMakeFiles/protoPersonalPlanner.dir/*/*.gc*
+# Using ln -s creates broken links and moving the files forces a rebuild to regenerate 
+# files built at compile time.
+find . -type f -name "*.gc*" -exec cp -t TestCoverage {} +
 
 echo "Running Lcov"
-lcov --directory . --capture --output-file TestCoverage/protoPersonalPlanner_Total.info --ignore-errors gcov,mismatch 2>&1 > lcovOut.txt
-echo "Removing External Coverage" 
-lcov --remove TestCoverage/protoPersonalPlanner_Total.info '/usr/include/*' '*/boost/*' -o TestCoverage/protoPersonalPlanner_filtered.info 2>&1 >> lcovOut.txt
-genhtml TestCoverage/protoPersonalPlanner_filtered.info --output-directory TestCoverage 2>&1 > genhtmlOut.txt
+lcov --directory . --capture --output-file TestCoverage/protoPersonalPlanner_Total.info --ignore-errors gcov,mismatch 2>&1 > TestCoverage/lcovOut.txt
+echo "Removing Library Coverage" 
+lcov --remove TestCoverage/protoPersonalPlanner_Total.info '/usr/include/*' '*/boost/*' -o TestCoverage/protoPersonalPlanner_filtered.info 2>&1 >> TestCoverage/lcovOut.txt
+echo "Generating test coverage report"
+genhtml TestCoverage/protoPersonalPlanner_filtered.info --output-directory TestCoverage 2>&1 > TestCoverage/genhtmlOut.txt
