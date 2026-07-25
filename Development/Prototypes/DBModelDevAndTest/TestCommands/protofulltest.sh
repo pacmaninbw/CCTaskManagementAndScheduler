e07ea261d8a4f7f62b9100ded647fcbbff30b528
pacmaninbw <paul.chernick@chernicksw.com>
2026-07-25 15:01:29 -0700

    Update the full test with the lates improvements. Corrected previous errors that prevented full testing.

--- Development/Prototypes/DBModelDevAndTest/TestCommands/protofulltest.sh
+++ Development/Prototypes/DBModelDevAndTest/TestCommands/protofulltest.sh
@@ -5,26 +5,23 @@
 set -e
 sqluser="${1:-no_username_supplied}"
 sqlpassword="${2:-no_password_supplied}"
-build_dir=build/CMakeFiles/protoPersonalPlanner.dir
 
 # Remove previously generated files
-rm -f $build_dir/*.gcda $build_dir/{common,Models,Testing}/*.gcda combinedInput.sql
 rm -rf TestCoverage
+find . -type f -name "*.gcda" -delete
 
-#
-# Combine the data definitions with the test data and create the test database
-#
-cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql > combinedInput.sql
-mysql -u $sqluser -p$sqlpassword < combinedInput.sql
+echo "Creating test database with test data"
+cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql | mysql -u $sqluser -p$sqlpassword
 
 # Run the basic regression test
 protoPersonalPlanner -u $sqluser -p $sqlpassword 2>&1 > Testing/testOut.txt
 
-# Reset the test database because the previous set altered the database
-mysql -u $sqluser -p$sqlpassword < combinedInput.sql
+# Reset the test database because the previous test altered the database
+cat PlannerTaskScheduleDB.sql AdditionalFunctionalTestData.sql | mysql -u $sqluser -p$sqlpassword
 
 # Run the valgrind regression test
-valgrind --track-origins=yes protoPersonalPlanner -u $sqluser -p $sqlpassword  2>&1 | sed 's/^==[0-9]*== //' > Testing/valgrindOut.txt
+valgrind --track-origins=yes protoPersonalPlanner -u "$sqluser" -p "$sqlpassword" --verbose --time-tests 2>&1 | 
+   sed 's/^==[0-9]*== //' > Testing/valgrindOut.txt
 echo "Diff"
 diff -w Testing/testOut.txt Testing/testOut_forDiff.txt > protoTestDiff.txt || true
 
@@ -33,12 +30,13 @@ diff Testing/valgrindOut.txt Testing/valgrindOut_forDiff.txt > valgrindTestDiff.
 
 mkdir TestCoverage
 echo "copy Gcov data"
-cp -t TestCoverage build/protoPersonalPlanner \
-    build/CMakeFiles/protoPersonalPlanner.dir/*.gc* \
-    build/CMakeFiles/protoPersonalPlanner.dir/*/*.gc*
-    
+# Using ln -s creates broken links and moving the files forces a rebuild to regenerate 
+# files built at compile time.
+find . -type f -name "*.gc*" -exec cp -t TestCoverage {} +
+
 echo "Running Lcov"
-lcov --directory . --capture --output-file TestCoverage/protoPersonalPlanner_Total.info --ignore-errors gcov,mismatch 2>&1 > lcovOut.txt
-echo "Removing External Coverage" 
-lcov --remove TestCoverage/protoPersonalPlanner_Total.info '/usr/include/*' '*/boost/*' -o TestCoverage/protoPersonalPlanner_filtered.info 2>&1 >> lcovOut.txt
-genhtml TestCoverage/protoPersonalPlanner_filtered.info --output-directory TestCoverage 2>&1 > genhtmlOut.txt
+lcov --directory . --capture --output-file TestCoverage/protoPersonalPlanner_Total.info --ignore-errors gcov,mismatch 2>&1 > TestCoverage/lcovOut.txt
+echo "Removing Library Coverage" 
+lcov --remove TestCoverage/protoPersonalPlanner_Total.info '/usr/include/*' '*/boost/*' -o TestCoverage/protoPersonalPlanner_filtered.info 2>&1 >> TestCoverage/lcovOut.txt
+echo "Generating test coverage report"
+genhtml TestCoverage/protoPersonalPlanner_filtered.info --output-directory TestCoverage 2>&1 > TestCoverage/genhtmlOut.txt
