4aca96deac69b682a592b31fa0361a8630f1913a
pacmaninbw <paul.chernick@chernicksw.com>
2026-08-20 07:43:32 -0700

    Added getHourRangeForLocalDate which moves the start time end end time code for the local day to a common function. Moved all extern symbols in commonUtilities into the common namespace.

--- Development/Prototypes/DBModelDevAndTest/common/commonUtilities.h
+++ Development/Prototypes/DBModelDevAndTest/common/commonUtilities.h
@@ -3,14 +3,20 @@
 #include <chrono>
 #include <string>
 
-extern unsigned int OneWeek;
-extern unsigned int TwoWeeks;
+namespace common
+{
+    extern unsigned int OneWeek;
+    extern unsigned int TwoWeeks;
 
-extern std::chrono::year_month_day getTodaysDate();
-extern std::chrono::year_month_day getTodaysDatePlus(unsigned int offset);
-extern std::chrono::year_month_day getTodaysDateMinus(unsigned int offset);
-extern std::chrono::year_month_day constantStringToChronoDate(std::string dateStr);
-extern std::chrono::system_clock::time_point constantStringToChronoTimePoint(std::string dateTimeStr);
-extern std::chrono::system_clock::time_point getLocalMidnight(std::chrono::year_month_day scheduleDate);
+    extern std::chrono::year_month_day getTodaysDate();
+    extern std::chrono::year_month_day getTodaysDatePlus(unsigned int offset);
+    extern std::chrono::year_month_day getTodaysDateMinus(unsigned int offset);
+    extern std::chrono::year_month_day constantStringToChronoDate(std::string dateStr);
+    extern std::chrono::system_clock::time_point constantStringToChronoTimePoint(std::string dateTimeStr);
+    extern std::chrono::system_clock::time_point getLocalMidnight(std::chrono::year_month_day scheduleDate);
+    extern void getHourRangeForLocalDate(std::chrono::year_month_day dateIn,
+        std::chrono::system_clock::time_point& startTime,
+        std::chrono::system_clock::time_point& endTime);
+}
 
 #endif // COMMONUTILITIES_H_
