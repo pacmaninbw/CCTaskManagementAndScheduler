5e70b4370fbc117bdd595d0560a41d67f211156b
pacmaninbw <paul.chernick@chernicksw.com>
2026-09-01 07:13:50 -0700

    Used lambda functions to replace static function in factory. Cited providor of the lambda function.

--- Development/Prototypes/DBModelDevAndTest/Testing/TestOrganizationModel.cpp
+++ Development/Prototypes/DBModelDevAndTest/Testing/TestOrganizationModel.cpp
@@ -1,8 +1,13 @@
 // Project Header Files
+#include "chronoBoostConversions.h"
+#include "commonUtilities.h"
 #include "OrganizationModel.h"
 #include "TestOrganizationModel.h"
+#include "UserQueryProcessor.h"
 
 // Standard C++ Header Files
+#include <chrono>
+#include <format>
 #include <functional>
 #include <string>
 #include <vector>
@@ -11,10 +16,80 @@
 TestOrganizationModel::TestOrganizationModel()
 : TestDBInterfaceCore("Organization")
 {
+    UserQueryProcessor userQueryProcessor;
+    m_userOne = userQueryProcessor.getUserByFullName("One", "User", "P");
+    if (m_userOne == nullptr || !m_userOne->isInDataBase())
+    {
+        std::cerr << std::format("Failed to find userOne in DB! : {}\n", userQueryProcessor.getAllErrorMessages());
+    }
 
+    m_userTwo = userQueryProcessor.getUserByFullName("Doe", "John", "Q");
+    if (m_userTwo == nullptr || !m_userTwo->isInDataBase())
+    {
+        std::cerr << std::format("Failed to find userTwo in DB! : {}\n", userQueryProcessor.getAllErrorMessages());
+    }
 }
 
 TestOrganizationModel::~TestOrganizationModel()
 {
 
 }
+
+OrganizationModel_shp TestOrganizationModel::organizationFactory(
+    std::string orgName,
+    std::string email,
+    std::string phoneNumber,
+    std::shared_ptr<UserModel> primaryContact,
+    std::shared_ptr<UserModel> secondaryContact,
+    std::optional<std::chrono::system_clock::time_point> created,
+    std::optional<std::chrono::system_clock::time_point> lastModified,
+    std::string addressl1,
+    std::string addressl2,
+    std::string city,
+    std::string zipCode,
+    std::string state,
+    std::string nation
+) noexcept
+{
+    OrganizationModel_shp newOrganization = std::make_shared<OrganizationModel>();
+
+    newOrganization->setOrganizationName(orgName);
+    newOrganization->setEmailAddress(email);
+    newOrganization->setPhoneNumber(phoneNumber);
+    newOrganization->setPrimaryContactUserId(primaryContact->getUserID());
+    newOrganization->setSecondaryContactUserId(secondaryContact->getUserID());
+
+    auto setTimeStampIfValid = [](OrganizationModel& object, auto setter, std::optional<std::chrono::system_clock::time_point> optionalTime) {
+        if (optionalTime.has_value())
+        {
+            std::invoke(setter, object, optionalTime.value());
+        }
+    };
+
+    setTimeStampIfValid(*newOrganization, &OrganizationModel::setCreationTimeStamp, created);
+    setTimeStampIfValid(*newOrganization, &OrganizationModel::setLastModified, lastModified);
+
+    // Source - https://stackoverflow.com/a/79999780
+    // Posted by JaMiT
+    // Retrieved 2026-09-01, License - CC BY-SA 4.0
+    auto conditionalSetField = [](OrganizationModel& object, auto setter, std::string& str) {
+        if (!str.empty())
+        {
+            std::invoke(setter, object, str);
+        }
+    };
+    
+    conditionalSetField(*newOrganization, &OrganizationModel::setAddressLine1, addressl1);
+    conditionalSetField(*newOrganization, &OrganizationModel::setAddressLine2, addressl2);
+    conditionalSetField(*newOrganization, &OrganizationModel::setCity, city);
+    conditionalSetField(*newOrganization, &OrganizationModel::setPostalCode, zipCode);
+    conditionalSetField(*newOrganization, &OrganizationModel::setStateOrProvince, state);
+    conditionalSetField(*newOrganization, &OrganizationModel::setNation, nation);
+
+    return newOrganization;
+}
+
+void TestOrganizationModel::creatTestOrganizations() noexcept
+{
+}
+
--- Development/Prototypes/DBModelDevAndTest/Testing/TestOrganizationModel.h
+++ Development/Prototypes/DBModelDevAndTest/Testing/TestOrganizationModel.h
@@ -1,12 +1,18 @@
 #ifndef TESTORGANIZATIONMODEL_H_
 #define TESTORGANIZATIONMODEL_H_
 
+class UserModel;
+
 // Project Header Files
+#include "commonTestValues.h"
 #include "TestDBInterfaceCore.h"
 #include "OrganizationModel.h"
 
 // Standard C++ Header Files
+#include <chrono>
 #include <functional>
+#include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -17,7 +23,19 @@ public:
     ~TestOrganizationModel();
 
 private:
+    OrganizationModel_shp organizationFactory(
+        std::string orgName, std::string email, std::string phoneNumber, std::shared_ptr<UserModel> primaryContact,
+        std::shared_ptr<UserModel> secondaryContact, 
+        std::optional<std::chrono::system_clock::time_point> created,
+        std::optional<std::chrono::system_clock::time_point> lastModified,
+        std::string addressl1 = "", std::string addressl2 = "", 
+        std::string city = "", std::string zipCode = "", std::string state = "", std::string nation = ""
+    ) noexcept;
+    void creatTestOrganizations() noexcept;
 
+    std::shared_ptr<UserModel> m_userOne;
+    std::shared_ptr<UserModel> m_userTwo;
+    std::vector<OrganizationModel_shp> m_testData;
 };
 
 #endif // TESTORGANIZATIONMODEL_H_
